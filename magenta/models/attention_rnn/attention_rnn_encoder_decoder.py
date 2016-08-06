323a8abd3dd6a9ffe0f8f2943914de9830f01c12
Eric Nichols <epnichols@gmail.com>
2016-08-06 16:30:08 -0400

    Simplifying binary time counters, and making consistent between attention_rnn and lookback_rnn. Also added "n = len(melody) - 1" to attention_rnn because I think it was missing.

--- magenta/models/attention_rnn/attention_rnn_encoder_decoder.py
+++ magenta/models/attention_rnn/attention_rnn_encoder_decoder.py
@@ -24,7 +24,6 @@ NO_EVENT = melodies_lib.NO_EVENT
 MIN_MIDI_PITCH = melodies_lib.MIN_MIDI_PITCH
 NOTES_PER_OCTAVE = melodies_lib.NOTES_PER_OCTAVE
 STEPS_PER_BAR = 16  # This code assumes the melodies have 16 steps per bar.
-
 MIN_NOTE = 48  # Inclusive
 MAX_NOTE = 84  # Exclusive
 TRANSPOSE_TO_KEY = 0  # C Major
@@ -33,6 +32,7 @@ TRANSPOSE_TO_KEY = 0  # C Major
 # in the note range.
 NUM_SPECIAL_INPUTS = 14 + NOTES_PER_OCTAVE * 2
 NUM_SPECIAL_CLASSES = 2
+NUM_BINARY_TIME_COUNTERS = 7
 
 
 class MelodyEncoderDecoder(melodies_lib.MelodyEncoderDecoder):
@@ -131,14 +131,10 @@ class MelodyEncoderDecoder(melodies_lib.MelodyEncoderDecoder):
         melody.events[-1] == melody.events[-(2 * STEPS_PER_BAR + 1)]):
       input_[self.note_range + 5] = 1.0
 
-    # Time keeping toggles.
-    input_[self.note_range + 6] = 1.0 if len(melody) % 2 else -1.0
-    input_[self.note_range + 7] = 1.0 if len(melody) / 2 % 2 else -1.0
-    input_[self.note_range + 8] = 1.0 if len(melody) / 4 % 2 else -1.0
-    input_[self.note_range + 9] = 1.0 if len(melody) / 8 % 2 else -1.0
-    input_[self.note_range + 10] = 1.0 if len(melody) / 16 % 2 else -1.0
-    input_[self.note_range + 11] = 1.0 if len(melody) / 32 % 2 else -1.0
-    input_[self.note_range + 12] = 1.0 if len(melody) / 64 % 2 else -1.0
+    # Binary time counters.
+    n = len(melody) - 1
+    for i in range(NUM_BINARY_TIME_COUNTERS):
+      input_[self.note_range + 6 + i] = 1.0 if (n / 2 ** i) % 2 else -1.0
 
     # The next event is the start of a bar.
     if len(melody) % STEPS_PER_BAR == 0:
