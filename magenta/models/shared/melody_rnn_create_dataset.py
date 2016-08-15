34f84123897b81b754f3716ab515f09fd4cfa280
Elliot Waite <elliotwaite@gmail.com>
2016-08-14 22:27:48 -0700

    Add --log flag to melody_rnn_create_dataset.py.

--- magenta/models/shared/melody_rnn_create_dataset.py
+++ magenta/models/shared/melody_rnn_create_dataset.py
@@ -39,6 +39,9 @@ tf.app.flags.DEFINE_string('output_dir', None,
 tf.app.flags.DEFINE_float('eval_ratio', 0.0,
                           'Fraction of input to set aside for eval set. '
                           'Partition is randomly selected.')
+tf.app.flags.DEFINE_string('log', 'INFO',
+                           'The threshold for what messages will be logged '
+                           'DEBUG, INFO, WARN, ERROR, or FATAL.')
 
 
 class EncoderPipeline(pipeline.Pipeline):
@@ -94,7 +97,7 @@ def get_pipeline(melody_encoder_decoder):
 
 
 def run_from_flags(pipeline_instance):
-  tf.logging.set_verbosity(tf.logging.INFO)
+  tf.logging.set_verbosity(FLAGS.log)
   FLAGS.input = os.path.expanduser(FLAGS.input)
   FLAGS.output_dir = os.path.expanduser(FLAGS.output_dir)
   pipeline.run_pipeline_serial(
