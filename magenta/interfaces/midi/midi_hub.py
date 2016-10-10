746f22535132822fda650dd53ef21b0b169536f8
Adam Roberts <adarob@google.com>
2016-10-10 13:36:12 -0700

    Fix lint errors.

--- magenta/interfaces/midi/midi_hub.py
+++ magenta/interfaces/midi/midi_hub.py
@@ -782,18 +782,16 @@ class MidiHub(object):
     self._metronome = None
 
     # Open MIDI ports.
-    self._inport = (input_midi_port
-                    if isinstance(input_midi_port, mido.ports.BaseInput)
-                    else mido.open_input(
-		        input_midi_port, 
-                        virtual=input_midi_port not in 
-                                get_available_input_ports()))
-    self._outport = (output_midi_port
-                     if isinstance(output_midi_port, mido.ports.BaseOutput)
-                     else mido.open_output(
-		         output_midi_port, 
-                         virtual=output_midi_port not in 
-                                 get_available_output_ports()))
+    self._inport = (
+        input_midi_port if isinstance(input_midi_port, mido.ports.BaseInput)
+        else mido.open_input(
+            input_midi_port,
+            virtual=input_midi_port not in get_available_input_ports()))
+    self._outport = (
+        output_midi_port if isinstance(output_midi_port, mido.ports.BaseOutput)
+        else mido.open_output(
+          output_midi_port,
+          virtual=output_midi_port not in get_available_output_ports()))
 
     # Start processing incoming messages.
     self._inport.callback = self._timestamp_and_handle_message
