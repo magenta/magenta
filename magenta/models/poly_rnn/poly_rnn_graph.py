664a3ec1c72925379b58e85d63f18aeba5517dad
Curtis Hawthorne <fjord@google.com>
2016-10-21 16:23:52 -0700

    training works again

--- magenta/models/poly_rnn/poly_rnn_graph.py
+++ magenta/models/poly_rnn/poly_rnn_graph.py
@@ -8,19 +8,19 @@ from tfkdllib import LSTM, LSTMFork
 
 class Graph(object):
   def __init__(self, examples):
-    num_epochs = 500
+    self.num_epochs = 500
     self.batch_size = 32
     # ~1 step per seconds
     # 30 steps ~= 30 seconds
     sequence_length = 30
 
-    train_itr = tfrecord_duration_and_pitch_iterator(examples,
+    self.train_itr = tfrecord_duration_and_pitch_iterator(examples,
                                                      self.batch_size,
                                                      stop_index=.9,
                                                      sequence_length=sequence_length)
 
-    duration_mb, note_mb = next(train_itr)
-    train_itr.reset()
+    duration_mb, note_mb = next(self.train_itr)
+    self.train_itr.reset()
 
     self.valid_itr = tfrecord_duration_and_pitch_iterator(examples,
                                                      self.batch_size,
@@ -29,9 +29,9 @@ class Graph(object):
 
     num_note_features = note_mb.shape[-1]
     num_duration_features = duration_mb.shape[-1]
-    n_note_symbols = len(train_itr.note_classes)
-    n_duration_symbols = len(train_itr.time_classes)
-    self.n_notes = train_itr.simultaneous_notes
+    n_note_symbols = len(self.train_itr.note_classes)
+    n_duration_symbols = len(self.train_itr.time_classes)
+    self.n_notes = self.train_itr.simultaneous_notes
     note_embed_dim = 20
     duration_embed_dim = 4
     n_dim = 64
@@ -157,10 +157,10 @@ class Graph(object):
         costs.append(cost)
 
     # 4 notes pitch and 4 notes duration
-    cost = sum(costs) / float(self.n_notes + self.n_notes)
+    self.cost = sum(costs) / float(self.n_notes + self.n_notes)
 
     params = tf.trainable_variables()
-    grads = tf.gradients(cost, params)
+    grads = tf.gradients(self.cost, params)
     grads = [tf.clip_by_value(grad, -grad_clip, grad_clip) for grad in grads]
     opt = tf.train.AdamOptimizer(learning_rate, use_locking=True)
-    updates = opt.apply_gradients(zip(grads, params))
+    self.updates = opt.apply_gradients(zip(grads, params))
--- magenta/models/poly_rnn/poly_rnn_train.py
+++ magenta/models/poly_rnn/poly_rnn_train.py
@@ -3,36 +3,38 @@ import tensorflow as tf
 import numpy as np
 from tfkdllib import run_loop
 from tfkdllib import tfrecord_duration_and_pitch_iterator
+import poly_rnn_graph
+import functools
 
 FLAGS = tf.app.flags.FLAGS
 tf.app.flags.DEFINE_string('input', None, 'Polyphonic tfrecord file')
 
 
-def _loop(itr, sess, inits=None, do_updates=True):
-    i_h1 = np.zeros((batch_size, rnn_dim)).astype("float32")
+def _loop(graph, itr, sess, inits=None, do_updates=True):
+    i_h1 = np.zeros((graph.batch_size, graph.rnn_dim)).astype("float32")
     duration_mb, note_mb = next(itr)
     X_note_mb = note_mb[:-1]
     y_note_mb = note_mb[1:]
     X_duration_mb = duration_mb[:-1]
     y_duration_mb = duration_mb[1:]
-    feed = {note_inpt: X_note_mb,
-            note_target: y_note_mb,
-            duration_inpt: X_duration_mb,
-            duration_target: y_duration_mb,
-            init_h1: i_h1}
+    feed = {graph.note_inpt: X_note_mb,
+            graph.note_target: y_note_mb,
+            graph.duration_inpt: X_duration_mb,
+            graph.duration_target: y_duration_mb,
+            graph.init_h1: i_h1}
     if do_updates:
-        outs = [cost, final_h1, updates]
+        outs = [graph.cost, graph.final_h1, graph.updates]
         train_loss, h1_l, _ = sess.run(outs, feed)
     else:
-        outs = [cost, final_h1]
+        outs = [graph.cost, graph.final_h1]
         train_loss, h1_l = sess.run(outs, feed)
     return train_loss, h1_l
 
 
 def main(unused_argv):
-  build_graph(FLAGS.input)
-  run_loop(_loop, train_itr, valid_itr,
-           n_epochs=num_epochs,
+  graph = poly_rnn_graph.Graph(FLAGS.input)
+  run_loop(functools.partial(_loop, graph), graph.train_itr, graph.valid_itr,
+           n_epochs=graph.num_epochs,
            checkpoint_delay=40,
            checkpoint_every_n_epochs=5)
 
