24274656f9c471223668d7d4d4f32cea5e4c6399
Adam Roberts <adarob@google.com>
2016-10-25 10:30:15 -0700

    Add optional filter to remove drum tracks when quantizing. (#291)
    * Add optional filter to remove drum tracks when quantizing.
    
    * Add test.

--- magenta/pipelines/pipelines_common.py
+++ magenta/pipelines/pipelines_common.py
@@ -33,17 +33,19 @@ from magenta.protobuf import music_pb2
 class Quantizer(pipeline.Pipeline):
   """A Module that quantizes NoteSequence data."""
 
-  def __init__(self, steps_per_quarter=4):
+  def __init__(self, steps_per_quarter=4, filter_drums=True):
     super(Quantizer, self).__init__(
         input_type=music_pb2.NoteSequence,
         output_type=sequences_lib.QuantizedSequence)
-    self.steps_per_quarter = steps_per_quarter
+    self._steps_per_quarter = steps_per_quarter
+    self._filter_drums = filter_drums
 
   def transform(self, note_sequence):
     quantized_sequence = sequences_lib.QuantizedSequence()
     try:
       quantized_sequence.from_note_sequence(note_sequence,
-                                            self.steps_per_quarter)
+                                            self._steps_per_quarter,
+                                            self._filter_drums)
       return [quantized_sequence]
     except sequences_lib.MultipleTimeSignatureException:
       tf.logging.debug('Multiple time signatures found in NoteSequence')
