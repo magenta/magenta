d022551cf39b5220c03b18d35452c11b98dce458
Curtis Hawthorne <fjord@google.com>
2016-10-26 15:59:15 -0700

    lint

--- magenta/models/poly_rnn/poly_rnn_generate.py
+++ magenta/models/poly_rnn/poly_rnn_generate.py
@@ -12,14 +12,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import tensorflow as tf
-import numpy as np
 import os
 import time
-from poly_rnn_lib import numpy_softmax, numpy_sample_softmax
-from poly_rnn_lib import duration_and_pitch_to_midi
-from poly_rnn_lib import tfrecord_duration_and_pitch_iterator
-import poly_rnn_graph
+
+# internal imports
+
+import numpy as np
+from magenta.model.polyrnn import poly_rnn_graph
+from magenta.model.polyrnn import poly_rnn_lib
+import tensorflow as tf
 
 FLAGS = tf.app.flags.FLAGS
 tf.app.flags.DEFINE_string(
@@ -36,78 +37,77 @@ tf.app.flags.DEFINE_string(
     'or FATAL.')
 
 
-def sample(model_ckpt, runtime, note_sequence_input, sample_path, sample_len, temperature):
-    graph = poly_rnn_graph.Graph(note_sequence_input)
-    graph.valid_itr.reset()
-    duration_mb, note_mb = graph.valid_itr.next()
-    duration_and_pitch_to_midi(sample_path + "/gt_%i.mid" % runtime, duration_mb[:, 0], note_mb[:, 0])
+def sample(model_ckpt, runtime, note_sequence_input, sample_path, sample_len,
+           temperature):
+  graph = poly_rnn_graph.Graph(note_sequence_input)
+  graph.valid_itr.reset()
+  duration_mb, note_mb = graph.valid_itr.next()
+  poly_rnn_lib.duration_and_pitch_to_midi(
+      sample_path + "/gt_%i.mid" % runtime, duration_mb[:, 0], note_mb[:, 0])
 
-    with tf.Session() as sess:
-        tf.initialize_all_variables().run()
-        saver = tf.train.Saver(tf.all_variables())
-        saver.restore(sess, model_ckpt)
-        i_h1 = np.zeros((graph.batch_size, graph.rnn_dim)).astype("float32")
+  with tf.Session() as sess:
+    tf.initialize_all_variables().run()
+    saver = tf.train.Saver(tf.all_variables())
+    saver.restore(sess, model_ckpt)
+    i_h1 = np.zeros((graph.batch_size, graph.rnn_dim)).astype("float32")
 
-        prime = 8
-        note_mb = note_mb[:prime]
-        duration_mb = duration_mb[:prime]
-        for n in range(duration_mb.shape[1]):
-            duration_and_pitch_to_midi(sample_path + "/pre%i_%i.mid" % (n, runtime),
-                                       duration_mb[:, n], note_mb[:, n], prime)
+    prime = 8
+    note_mb = note_mb[:prime]
+    duration_mb = duration_mb[:prime]
+    for n in range(duration_mb.shape[1]):
+        poly_rnn_lib.duration_and_pitch_to_midi(
+            sample_path + "/pre%i_%i.mid" % (n, runtime),
+            duration_mb[:, n], note_mb[:, n], prime)
 
-        note_inputs = note_mb
-        duration_inputs = duration_mb
+    note_inputs = note_mb
+    duration_inputs = duration_mb
 
-        """
-        note_inputs = np.zeros((1, batch_size, train_itr.simultaneous_notes))
-        duration_inputs = np.zeros((1, batch_size, train_itr.simultaneous_notes))
-        """
+    shp = note_inputs.shape
+    full_notes = np.zeros((sample_len, shp[1], shp[2]), dtype="float32")
+    full_notes[:len(note_inputs)] = note_inputs[:]
+    shp = duration_inputs.shape
+    full_durations = np.zeros((sample_len, shp[1], shp[2]), dtype="float32")
+    full_durations[:len(duration_inputs)] = duration_inputs[:]
 
-        shp = note_inputs.shape
-        full_notes = np.zeros((sample_len, shp[1], shp[2]), dtype="float32")
-        full_notes[:len(note_inputs)] = note_inputs[:]
-        shp = duration_inputs.shape
-        full_durations = np.zeros((sample_len, shp[1], shp[2]), dtype="float32")
-        full_durations[:len(duration_inputs)] = duration_inputs[:]
+    random_state = np.random.RandomState(1999)
+    for j in range(sample_len - 1):
+      # even predictions are note, odd are duration
+      for ni in range(2 * graph.n_notes):
+        feed = {graph.note_inpt: full_notes[j][None, :, :],
+                graph.note_target: full_notes[j + 1][None, :, :],
+                graph.duration_inpt: full_durations[j][None, :, :],
+                graph.duration_target: full_durations[j + 1][None, :, :],
+                graph.init_h1: i_h1}
+        outs = []
+        outs += graph.note_preds
+        outs += graph.duration_preds
+        outs += [graph.final_h1]
+        r = sess.run(outs, feed)
+        h_l = r[-1:]
+        h1_l = h_l[-1]
+        this_preds = r[:-1]
+        this_probs = [poly_rnn_lib.numpy_softmax(p, temperature=temperature)
+                      for p in this_preds]
+        this_samples = [poly_rnn_lib.numpy_sample_softmax(p, random_state)
+                        for p in this_probs]
+        if j < (len(note_inputs) - 1):
+          # bypass sampling for now - still in prime seq
+          continue
+        note_probs = this_probs[:graph.n_notes]
+        duration_probs = this_probs[graph.n_notes:]
+        si = ni // 2
+        if (ni % 2) == 0:
+          # only put the single note in...
+          full_notes[j + 1, :, si] = this_samples[si].ravel()
+        if (ni % 2) == 1:
+          full_durations[j + 1, :, si] = (
+              this_samples[si + graph.n_notes].ravel())
+      i_h1 = h1_l
 
-        random_state = np.random.RandomState(1999)
-        for j in range(sample_len - 1):
-            # even predictions are note, odd are duration
-            for ni in range(2 * graph.n_notes):
-                feed = {graph.note_inpt: full_notes[j][None, :, :],
-                        graph.note_target: full_notes[j + 1][None, :, :],
-                        graph.duration_inpt: full_durations[j][None, :, :],
-                        graph.duration_target: full_durations[j + 1][None, :, :],
-                        graph.init_h1: i_h1}
-                outs = []
-                outs += graph.note_preds
-                outs += graph.duration_preds
-                outs += [graph.final_h1]
-                r = sess.run(outs, feed)
-                h_l = r[-1:]
-                h1_l = h_l[-1]
-                this_preds = r[:-1]
-                this_probs = [numpy_softmax(p, temperature=temperature)
-                              for p in this_preds]
-                this_samples = [numpy_sample_softmax(p, random_state)
-                                for p in this_probs]
-                if j < (len(note_inputs) - 1):
-                    # bypass sampling for now - still in prime seq
-                    continue
-                note_probs = this_probs[:graph.n_notes]
-                duration_probs = this_probs[graph.n_notes:]
-                si = ni // 2
-                if (ni % 2) == 0:
-                    # only put the single note in...
-                    full_notes[j + 1, :, si] = this_samples[si].ravel()
-                if (ni % 2) == 1:
-                    full_durations[j + 1, :, si] = this_samples[si + graph.n_notes].ravel()
-            i_h1 = h1_l
-
-        for n in range(full_durations.shape[1]):
-            duration_and_pitch_to_midi(sample_path + "/sampled%i_%i.mid" % (n, runtime),
-                                       full_durations[:, n], full_notes[:, n],
-                                       prime)
+    for n in range(full_durations.shape[1]):
+      poly_rnn_lib.duration_and_pitch_to_midi(
+          sample_path + "/sampled%i_%i.mid" % (n, runtime),
+          full_durations[:, n], full_notes[:, n], prime)
 
 
 def main(unused_argv):
