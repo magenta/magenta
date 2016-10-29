dc1c46994ca0913ff46f925f02c35c6f9c0a59f4
Natasha Jaques <jaquesn@mit.edu>
2016-10-29 11:59:43 -0400

    remove scripts for testing

--- run_4_rl_rnn_algs.sh
+++
@@ -1,4 +0,0 @@
-bazel run magenta/models/rl_rnn:melody_q_train -- --algorithm 'ftq'
-bazel run magenta/models/rl_rnn:melody_q_train -- --algorithm 'g'
-bazel run magenta/models/rl_rnn:melody_q_train -- --algorithm 'psi'
-bazel run magenta/models/rl_rnn:melody_q_train -- --algorithm 'pure_rl'
