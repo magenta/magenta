2cbf00964fbc2771872f0cf026e7ee5f61d2e6a6
Curtis Hawthorne <fjord@google.com>
2016-11-03 11:14:17 -0700

    lint

--- magenta/music/musicxml_parser_test.py
+++ magenta/music/musicxml_parser_test.py
@@ -21,10 +21,10 @@ import os.path
 import tensorflow as tf
 
 from magenta.common import testing_lib as common_testing_lib
-from magenta.music import sequences_lib
-from magenta.music import testing_lib
 from magenta.music import musicxml_parser
 from magenta.music import musicxml_reader
+from magenta.music import sequences_lib
+from magenta.music import testing_lib
 from magenta.protobuf import music_pb2
 
 
