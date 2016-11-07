7318511a81d70e2f755fbb9c47f70c8e15feb58d
Natasha Jaques <jaquesn@mit.edu>
2016-11-07 13:31:18 -0500

    test something with constants

--- magenta/models/rl_tuner/rl_tuner_eval_metrics.py
+++ magenta/models/rl_tuner/rl_tuner_eval_metrics.py
@@ -3,6 +3,47 @@ import tensorflow as tf
 
 import rl_tuner
 
+
+# Music theory constants used in defining reward functions.
+# Note that action 2 = midi note 48.
+NOTE_OFF = 0
+NO_EVENT = 1
+C_MAJOR_SCALE = [2, 4, 6, 7, 9, 11, 13, 14, 16, 18, 19, 21, 23, 25, 26]
+C_MAJOR_KEY = [0, 1, 2, 4, 6, 7, 9, 11, 13, 14, 16, 18, 19, 21, 23, 25, 26, 28,
+               30, 31, 33, 35, 37]
+C_MAJOR_TONIC = 14
+A_MINOR_TONIC = 23
+
+# The number of half-steps in musical intervals, in order of dissonance
+OCTAVE = 12
+FIFTH = 7
+THIRD = 4
+SIXTH = 9
+SECOND = 2
+FOURTH = 5
+SEVENTH = 11
+HALFSTEP = 1
+
+# Special intervals that have unique rewards
+REST_INTERVAL = -1
+HOLD_INTERVAL = -1.5
+REST_INTERVAL_AFTER_THIRD_OR_FIFTH = -2
+HOLD_INTERVAL_AFTER_THIRD_OR_FIFTH = -2.5
+IN_KEY_THIRD = -3
+IN_KEY_FIFTH = -5
+
+# Indicate melody direction
+ASCENDING = 1
+DESCENDING = -1
+
+# Indicate whether a melodic leap has been resolved or if another leap was made
+LEAP_RESOLVED = 1
+LEAP_DOUBLED = -1
+
+# training data sequences are limited to this length, so the padding queue pads
+# to this length
+TRAIN_SEQUENCE_LENGTH = 192
+
 def compute_composition_stats(rl_tuner,
                               num_compositions=10000,
                               composition_length=32,
