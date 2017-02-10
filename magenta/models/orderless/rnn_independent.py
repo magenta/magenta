f5c7aa74fa589874ed99fa95a3d45baebde68aba
Daniel Ritchie <daniel.c.ritchie@gmail.com>
2017-02-09 18:44:57 -0800

    got basic RNN with independent note distributions to start training

--- magenta/models/orderless/rnn_independent.py
+++ magenta/models/orderless/rnn_independent.py
@@ -2,9 +2,9 @@ import tensorflow as tf
 
 # Load a padded batch of data from the training data file
 def loadBatch(opts):
-	filename = opts.filename
-	batchSize = opts.batch_size
-	num_threads = opts.num_threads
+	filename = opts['filename']
+	batchSize = opts['batch_size']
+	numThreads = opts['num_threads']
 	filenameQueue = tf.train.string_input_producer([filename])
 	reader = tf.TFRecordReader()
 	_, serializedExample = reader.read(filenameQueue)
@@ -51,12 +51,12 @@ def make_rnn_cell(rnn_layer_sizes,
 
 # Build the graph which evaluates training loss
 def trainingLoss(inputs, targets, lengths, opts):
-	cell = make_rnn_cell(opts.rnn_layer_sizes,
-						 dropout_keep_prob=opts.dropout_keep_prob,
-						 attn_length=opts.attn_length)
+	cell = make_rnn_cell(opts['rnn_layer_sizes'],
+						 dropout_keep_prob=opts['dropout_keep_prob'],
+						 attn_length=opts['attn_length'])
 
 	# Initialize hidden state to zero
-	initial_state = cell.zero_state(opts.batch_size, tf.float32)
+	initial_state = cell.zero_state(opts['batch_size'], tf.float32)
 
 	# Run the RNN
 	outputs, final_state = tf.nn.dynamic_rnn(
@@ -64,17 +64,19 @@ def trainingLoss(inputs, targets, lengths, opts):
 		swap_memory=True)
 
 	# Combine batch and time dimensions so we have a 2D tensor (i.e. a list of
-	#    of opts.num_notes-long tensors)
+	#    of opts.num_notes-long tensors). Need for layers.linear, I think?
 	outputs_flat = tf.reshape(outputs, [-1, cell.output_size])
-	targets_flat = tf.reshape(targets, [-1, opts.num_notes])
-	# Combine logits for sigmoid cross entropy loss
-	logits_flat = tf.contrib.layers.linear(outputs_flat, opts.num_notes)
-	num_logits = tf.to_float(tf.reduce_sum(lengths))
+	targets_flat = tf.reshape(targets, [-1, opts['num_notes']])
+	# Compute logits for sigmoid cross entropy loss
+	logits_flat = tf.contrib.layers.linear(outputs_flat, opts['num_notes'])
+	num_time_slices = tf.to_float(tf.reduce_sum(lengths))
 	# Mask out the stuff that was past the end of each training sequence (due to padding)
 	mask_flat = tf.reshape(tf.sequence_mask(lengths, dtype=tf.float32), [-1])
-	# Compute sigmoid cross entropy loss, sum across time and batch
+	# Compute sigmoid cross entropy loss
 	sce = tf.nn.sigmoid_cross_entropy_with_logits(logits_flat, targets_flat)
-	loss = tf.reduce_sum(mask_flat * sce) / num_logits
+	# Sum across 'space' (i.e. entries in a single time slice), then across time+batch
+	sce = tf.reduce_sum(sce, 1)
+	loss = tf.reduce_sum(mask_flat * sce) / num_time_slices
 
 	return loss
 
@@ -83,34 +85,30 @@ def trainingLoss(inputs, targets, lengths, opts):
 def trainingIteration(loss, opts):
 	global_step = tf.Variable(0, trainable=False)
 	learning_rate = tf.train.exponential_decay(
-			opts.initial_learning_rate, global_step, opts.decay_steps,
-			opts.decay_rate, staircase=True)
+			opts['initial_learning_rate'], global_step, opts['decay_steps'],
+			opts['decay_rate'], staircase=True)
 	opt = tf.train.AdamOptimizer(learning_rate)
 	params = tf.trainable_variables()
 	gradients = tf.gradients(loss, params)
 	clipped_gradients, _ = tf.clip_by_global_norm(gradients,
-												  opts.gradient_clip_norm)
+												  opts['gradient_clip_norm'])
 	train_op = opt.apply_gradients(zip(clipped_gradients, params),
 								   global_step)
 
-	return {
-		'train_op': train_op,
-		'learning_rate': learning_rate,
-		'global_step': global_step
-	}
+	return train_op, learning_rate, global_step
 
 # Assemble the complete graph and do training
 def doTraining(opts):
 	inputs, targets, lengths = loadBatch(opts)
 	loss = trainingLoss(inputs, targets, lengths, opts)
-	trainIter = trainingIteration(loss, opts)
+	train_op, learning_rate, global_step = trainingIteration(loss, opts)
 
-	# sv = tf.train.Supervisor(logdir=train_dir, save_model_secs=30,
-	# 					   global_step=global_step)
-	sv = tf.train.Supervisor(global_step=global_step)
+	sv = tf.train.Supervisor(logdir=opts['log_dir'], save_model_secs=opts['save_model_secs'],
+						   global_step=global_step)
 
+	tf.logging.set_verbosity(tf.logging.INFO)
 	with sv.managed_session() as sess:
-		global_step_ = sess.run(trainIter.global_step)
+		global_step_ = sess.run(global_step)
 		# if opts.n_training_iters and global_step_ >= opts.n_training_iters:
 		# 	tf.logging.info('This checkpoint\'s global_step value is already %d, '
 		# 				  'which is greater or equal to the specified '
@@ -118,19 +116,19 @@ def doTraining(opts):
 		# 				  global_step_, num_training_steps)
 		# 	return
 		tf.logging.info('Starting training loop...')
-		while global_step_ < opts.n_training_iters:
+		while global_step_ < opts['n_training_iters']:
 			if sv.should_stop():
 				break
-			if (global_step_ + 1) % opts.summary_frequency == 0:
-				(global_step_, learning_rate_, loss_, perplexity_, accuracy_, _) = \
+			if (global_step_ + 1) % opts['summary_frequency'] == 0:
+				(global_step_, learning_rate_, loss_, _) = \
 					sess.run([global_step, learning_rate, loss, train_op])
 				tf.logging.info('Global Step: %d - '
 								'Learning Rate: %.5f - '
-								'Loss: %.3f - '
+								'Loss: %.3f - ',
 								global_step_, learning_rate_, loss_)
 			else:
 				global_step_, _ = sess.run([global_step, train_op])
-		# sv.saver.save(sess, sv.save_path, global_step=sv.global_step)
+		sv.saver.save(sess, sv.save_path, global_step=sv.global_step)
 		tf.logging.info('Training complete.')
 
 # ---------------------------------------------------------------------------------------
@@ -154,6 +152,8 @@ opts = {
 	'decay_rate': 0.95,
 	'gradient_clip_norm': 5,
 	'summary_frequency': 10,
+	'log_dir': './log',
+	'save_model_secs': 30
 }
 
 doTraining(opts)
