f3894904913ad87e7c5fdb515d7df60112a97848
Maxime Voisin <maximev@stanford.edu>
2017-04-04 17:09:21 -0700

    Update __init__.py
    Enable RNNNade import

--- musicgen/common/models/__init__.py
+++ musicgen/common/models/__init__.py
@@ -1,3 +1,4 @@
 from common.models.model import Model
 from common.models.sequenceGenerativeModel import SequenceGenerativeModel
 from common.models.rnnIndependent import RNNIndependent
+from common.models.rnnNade import RNNNade
