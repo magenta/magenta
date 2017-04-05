0334cc89904586920c6f2409c14616478f8bf66b
Maxime Voisin <maximev@stanford.edu>
2017-04-04 17:20:02 -0700

    Update and rename RNNNade.py to rnnNade.py

--- musicgen/common/models/rnnNade.py
+++ musicgen/common/models/rnnNade.py
@@ -1,22 +1,23 @@
 import tensorflow as tf
 from common.models.sequenceGenerativeModel import SequenceGenerativeModel
+from common.distributions.Nade import NADE
 
 """
-An RNN that models a sequence of binary random vectors, where the components of each
-   vector are modeled independently from one other given the RNN hidden state.
+An RNN that models a sequence of NADE objects, where the parameters of NADE
+   are determined by the RNN hidden state.
 """
-class RNNIndependent(SequenceGenerativeModel):
+class RNNNade(SequenceGenerativeModel):
 
 	def __init__(self, hparams, sequence_encoder):
-		super(RNNIndependent, self).__init__(hparams, sequence_encoder)
+		super(RNNNade, self).__init__(hparams, sequence_encoder)
 
 	def get_step_dist(self, rnn_outputs, condition_dict):
 		# Combine batch and time dimensions so we have a 2D tensor (i.e. a list of
 		#    of opts.num_notes-long tensors). Need for layers.linear, I think?
 		outputs_flat = tf.reshape(rnn_outputs, [-1, self.rnn_cell().output_size])
 		# Compute logits for sigmoid cross entropy loss
-		logits_flat = tf.contrib.layers.linear(outputs_flat, self.timeslice_size)
-		# Create and return Bernoulli dist
+		#logits_flat = tf.contrib.layers.linear(outputs_flat, self.timeslice_size)
+		# Create and return NADE dist
 		# (sample dtype is float so samples can be fed right back into inputs)
-		dist = tf.contrib.distributions.Bernoulli(logits=logits_flat, dtype=tf.float32)
+		dist = NADE(#logits=logits_flat, dtype=tf.float32)
 		return dist
