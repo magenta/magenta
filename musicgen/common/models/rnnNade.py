6016d8bcb8dd320ed0c54800540b02e27871ee86
Maxime Voisin <maximev@stanford.edu>
2017-04-08 19:40:44 -0700

    Update rnnNade.py

--- musicgen/common/models/rnnNade.py
+++ musicgen/common/models/rnnNade.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 from common.models.sequenceGenerativeModel import SequenceGenerativeModel
-from common.distributions import Nade
+from common.distributions import NADE
 
 """
 An RNN that models a sequence of NADE objects, where the parameters of each NADE
@@ -12,6 +12,7 @@ class RNNNade(SequenceGenerativeModel):
 		super(RNNNade, self).__init__(hparams, sequence_encoder)
 		self.size_hidden_layer=size_hidden_layer
 
+
 	def get_step_dist(self, rnn_outputs, condition_dict):
 		with tf.variable_scope('NADE_model') as scope:
 			W = tf.get_variable("W", shape = (self.timeslice_size, self.size_hidden_layer), initializer = tf.contrib.layers.xavier_initializer())
@@ -22,10 +23,10 @@ class RNNNade(SequenceGenerativeModel):
 		#    of opts.num_notes-long tensors). Need for layers.linear, I think?
 		outputs_flat = tf.reshape(rnn_outputs, [-1, self.rnn_cell().output_size])
 		# Compute parameters a and b of NADE object
-		b = tf.contrib.layers.fully_connected(inputs = output_flat, num_outputs = self.timeslice_size, activation_fn = None,
+		b = tf.contrib.layers.fully_connected(inputs = outputs_flat, num_outputs = self.timeslice_size, activation_fn = None,
 			weights_initializer = tf.contrib.layers.xavier_initializer(), biases_initializer = tf.contrib.layers.xavier_initializer(),
 			reuse = True, trainable = True, scope = 'NADE_model/b')
-		a = tf.contrib.layers.fully_connected(inputs = output_flat, num_outputs = size_hidden_layer, activation_fn = None,
+		a = tf.contrib.layers.fully_connected(inputs = outputs_flat, num_outputs = self.size_hidden_layer, activation_fn = None,
 			weights_initializer = tf.contrib.layers.xavier_initializer(), biases_initializer = tf.contrib.layers.xavier_initializer(),
 			reuse = True, trainable = True, scope='NADE_model/a')
 
@@ -33,3 +34,6 @@ class RNNNade(SequenceGenerativeModel):
 		# (sample dtype is float so samples can be fed right back into inputs)
 		dist = NADE(a,b,W,V,dtype=tf.float32)
 		return dist
+
+	def eval_factor_function(self, sample, condition):
+		return 
