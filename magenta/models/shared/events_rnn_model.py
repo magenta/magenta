Fix pad_amt calculation.

magenta/models/shared/events_rnn_model.py
@@ -143,7 +143,7 @@ class EventSequenceRnnModel(mm.BaseModel):
     final_states = []
     loglik = np.empty(num_seqs)
 
-    pad_amt = len(event_sequences) % batch_size
+    pad_amt = -len(event_sequences) % batch_size
     padded_event_sequences = event_sequences + [
         copy.deepcopy(event_sequences[-1]) for _ in range(pad_amt)]
     padded_inputs = inputs + [inputs[-1]] * pad_amt