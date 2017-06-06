73578f5f8021295b848716b5009ed4222ffdf074
Curtis Hawthorne <fjord@google.com>
2017-06-06 15:54:30 -0700

    deps

--- kokoro/test.sh
+++ kokoro/test.sh
@@ -5,6 +5,8 @@ set -e
 # Display commands being run.
 set -x
 
+sudo pip install tensorflow scipy matplotlib intervaltree bokeh
+
 bazel test --test_lang_filters=py -k \
       --test_output=errors -- \
           //magenta/...
