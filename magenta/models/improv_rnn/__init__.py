f3a5c33be2581fac4c5162aee4ee90d92fdcd3a4
Curtis "Fjord" Hawthorne <curtis@thefjord.org>
2017-08-30 09:17:45 -0700

    fix import paths (#854)

--- magenta/models/improv_rnn/__init__.py
+++ magenta/models/improv_rnn/__init__.py
@@ -14,4 +14,8 @@
 
 """Imports Improv RNN model."""
 
-from improv_rnn_model import ImprovRnnModel
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from .improv_rnn_model import ImprovRnnModel
