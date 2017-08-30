b4ec1d3f0684e2dbd19c5c26032e732f792dd911
Curtis Hawthorne <fjord@google.com>
2017-08-29 17:26:23 -0700

    fix import paths

--- magenta/models/performance_rnn/__init__.py
+++ magenta/models/performance_rnn/__init__.py
@@ -14,4 +14,8 @@
 
 """Imports Performance RNN model."""
 
-from performance_model import PerformanceRnnModel
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from .performance_model import PerformanceRnnModel
--- magenta/models/pianoroll_rnn_nade/__init__.py
+++ magenta/models/pianoroll_rnn_nade/__init__.py
@@ -14,4 +14,8 @@
 
 """Imports Pianoroll RNN-NADE model."""
 
-from pianoroll_rnn_nade_model import PianorollRnnNadeModel
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from .pianoroll_rnn_nade_model import PianorollRnnNadeModel
