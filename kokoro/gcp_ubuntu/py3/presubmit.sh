bc45996f7c74126f007a8ffe8ac64f7cd999ef22
Curtis Hawthorne <fjord@google.com>
2017-09-14 14:59:58 -0700

    fix py3 env

--- kokoro/gcp_ubuntu/py3/presubmit.sh
+++ kokoro/gcp_ubuntu/py3/presubmit.sh
@@ -5,10 +5,13 @@ set -e
 # Display commands to stderr.
 set -x
 
+# Set up Python 3.6.1 environment.
+eval 'pyenv shell 3.6.1'
+export PIP_COMMAND='python3.6 -m pip'
 # Ensure that python 3 is used.
 # Filter out tests that support only python 2.
-export PIP='pip3'
-export BAZEL_TEST_ARGS='--force_python=py3  --test_tag_filters=-py2only --build_tag_filters=-py2only'
+export BAZEL_TEST_ARGS='--force_python=py3  --test_tag_filters=-py2only \
+  --build_tag_filters=-py2only'
 
 cd github/magenta
 kokoro/test.sh
