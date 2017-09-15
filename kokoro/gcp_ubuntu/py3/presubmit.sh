ea74519cda596039be9bb216df799ea0eec5c80a
Curtis Hawthorne <fjord@google.com>
2017-09-15 09:44:32 -0700

    fix

--- kokoro/gcp_ubuntu/py3/presubmit.sh
+++ kokoro/gcp_ubuntu/py3/presubmit.sh
@@ -7,7 +7,7 @@ set -x
 
 # Ensure python 3.5 is used.
 PY3_PATH="$(which python3.5)"
-export PIP_COMMAND="${PY3_PATH} -m pip"
+export PIP_COMMAND="sudo ${PY3_PATH} -m pip"
 # Filter out tests that support only python 2.
 export BAZEL_TEST_ARGS="--force_python=py3  --test_tag_filters=-py2only \
   --build_tag_filters=-py2only --python_path=${PY3_PATH}"
