d4b502a37090435378b510be7b26bc3be15521c8
Adam Roberts <adarob@google.com>
2017-12-12 11:57:31 -0800

    Add merge_hparams to common.

--- magenta/common/__init__.py
+++ magenta/common/__init__.py
@@ -22,3 +22,4 @@ from .sequence_example_lib import count_records
 from .sequence_example_lib import flatten_maybe_padded_sequences
 from .sequence_example_lib import get_padded_batch
 from .sequence_example_lib import make_sequence_example
+from .tf_utils import merge_hparams
