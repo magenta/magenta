a2821bdff4b0fbc652bc827516ef5471b7530225
Curtis Hawthorne <fjord@google.com>
2018-02-26 15:48:31 -0800

    add functionality

--- magenta/js/src/sequences.ts
+++ magenta/js/src/sequences.ts
@@ -16,8 +16,94 @@
  */
 
 import music_pb = require('./music_pb');
+import * as constants from './constants';
+
+class MultipleTimeSignatureException extends Error {}
+class BadTimeSignatureException extends Error {}
+class NegativeTimeException extends Error {}
+
+// Set the quantization cutoff.
+// Note events before this cutoff are rounded down to nearest step. Notes
+// above this cutoff are rounded up to nearest step. The cutoff is given as a
+// fraction of a step.
+// For example, with quantize_cutoff = 0.75 using 0-based indexing,
+// if .75 < event <= 1.75, it will be quantized to step 1.
+// If 1.75 < event <= 2.75 it will be quantized to step 2.
+// A number close to 1.0 gives less wiggle room for notes that start early,
+// and they will be snapped to the previous step.
+const QUANTIZE_CUTOFF = 0.5
 
 export class Sequences {
+    private static isPowerOf2(n) {
+        return n && (n & (n - 1)) === 0;
+    }
+
+    /**
+     * Calculates steps per second given stepsPerQuarter and a QPM.
+     */
+    public static stepsPerQuarterToStepsPerSecond(stepsPerQuarter, qpm) {
+        return stepsPerQuarter * qpm / 60.0;
+    }
+
+    /**
+     * Quantizes seconds to the nearest step, given steps_per_second.
+     * See the comments above `QUANTIZE_CUTOFF` for details on how the quantizing
+     * algorithm works.
+     * @param unquantizedSeconds Seconds to quantize.
+     * @param stepsPerSecond Quantizing resolution.
+     * @param quantizeCutoff Value to use for quantizing cutoff.
+     */
+    public static quantizeToStep(unquantizedSeconds, stepsPerSecond,
+            quantizeCutoff=QUANTIZE_CUTOFF) {
+        var unquantizedSteps = unquantizedSeconds * stepsPerSecond;
+        return Math.floor(unquantizedSteps + (1 - quantizeCutoff));
+    }
+
+    /**
+     * Quantize the notes and chords of a NoteSequence proto in place.
+
+     * Note start and end times, and chord times are snapped to a nearby quantized
+     * step, and the resulting times are stored in a separate field (e.g.,
+     * QuantizedStartStep). See the comments above `QUANTIZE_CUTOFF` for details on
+     * how the quantizing algorithm works.
+     * @param ns A music_pb2.NoteSequence protocol buffer. Will be modified in place.
+     * @param stepsPerSecond Each second will be divided into this many quantized time steps.
+     */
+    private static quantizeNotes(ns, stepsPerSecond) {
+        for(var note of ns.getNotesList()) {
+            // Quantize the start and end times of the note.
+            note.setQuantizedStartStep(this.quantizeToStep(
+                note.getStartTime(), stepsPerSecond));
+            note.setQuantizedEndStep(this.quantizeToStep(
+                note.getEndTime(), stepsPerSecond));
+            if(note.getQuantizedEndStep() === note.getQuantizedStartStep()) {
+                note.setQuantizedEndStep(note.getQuantizedEndStep() + 1);
+            }
+
+            // Do not allow notes to start or end in negative time.
+            if(note.getQuantizedStartStep() < 0 || note.getQuantizedEndStep() < 0) {
+                throw new NegativeTimeException(
+                    'Got negative note time: start_step = ' +
+                    note.getQuantizedStartStep() + ', end_step = ' +
+                    note.getQuantizedEndStep());
+            }
+
+            // Extend quantized sequence if necessary.
+            if(note.getQuantizedEndStep() > ns.getTotalQuantizedSteps()) {
+                ns.setTotalQuantizedSteps(note.getQuantizedEndStep());
+            }
+        }
+
+        // Also quantize control changes and text annotations.
+        // for event in itertools.chain(
+        //     note_sequence.control_changes, note_sequence.text_annotations):
+        //     // Quantize the event time, disallowing negative time.
+        //     event.quantized_step = quantize_to_step(event.time, steps_per_second)
+        //     if event.quantized_step < 0:
+        //     raise NegativeTimeException(
+        //         'Got negative event time: step = %s' % event.quantized_step)
+    }
+
     /**
      * Quantize a NoteSequence proto relative to tempo.
      *
@@ -27,7 +113,7 @@ export class Sequences {
 
      * Note start and end times, and chord times are snapped to a nearby quantized
      * step, and the resulting times are stored in a separate field (e.g.,
-     * quantized_start_step). See the comments above `QUANTIZE_CUTOFF` for details on
+     * QuantizedStartStep). See the comments above `QUANTIZE_CUTOFF` for details on
      * how the quantizing algorithm works.
      *
      * Args:
@@ -52,51 +138,62 @@ export class Sequences {
         qns.setQuantizationInfo(new music_pb.NoteSequence.QuantizationInfo())
         qns.getQuantizationInfo().setStepsPerQuarter(stepsPerQuarter);
 
-        // if qns.time_signatures:
-        //     time_signatures = sorted(qns.time_signatures, key=lambda ts: ts.time)
-        //     # There is an implicit 4/4 time signature at 0 time. So if the first time
-        //     # signature is something other than 4/4 and it's at a time other than 0,
-        //     # that's an implicit time signature change.
-        //     if time_signatures[0].time != 0 and not (
-        //         time_signatures[0].numerator == 4 and
-        //         time_signatures[0].denominator == 4):
-        //     raise MultipleTimeSignatureException(
-        //         'NoteSequence has an implicit change from initial 4/4 time '
-        //         'signature to %d/%d at %.2f seconds.' % (
-        //             time_signatures[0].numerator, time_signatures[0].denominator,
-        //             time_signatures[0].time))
+        if(qns.getTimeSignaturesList().length > 0) {
+            var timeSignatures = qns.getTimeSignaturesList();
+            timeSignatures.sort(function(a, b) {
+                return a.getTime() - b.getTime();
+            });
+            // There is an implicit 4/4 time signature at 0 time. So if the first time
+            // signature is something other than 4/4 and it's at a time other than 0,
+            // that's an implicit time signature change.
+            if(timeSignatures[0].getTime() != 0 && !(
+                    timeSignatures[0].getNumerator() == 4 &&
+                    timeSignatures[0].getDenominator() == 4)) {
+                throw new MultipleTimeSignatureException(
+                    'NoteSequence has an implicit change from initial 4/4 time ' +
+                    'signature to ' + timeSignatures[0].getNumerator() + '/' +
+                    timeSignatures[0].getDenominator() + ' at ' +
+                    timeSignatures[0].getTime() + ' seconds.');
+            }
 
-        //     for time_signature in time_signatures[1:]:
-        //     if (time_signature.numerator != qns.time_signatures[0].numerator or
-        //         time_signature.denominator != qns.time_signatures[0].denominator):
-        //         raise MultipleTimeSignatureException(
-        //             'NoteSequence has at least one time signature change from %d/%d to '
-        //             '%d/%d at %.2f seconds.' % (
-        //                 time_signatures[0].numerator, time_signatures[0].denominator,
-        //                 time_signature.numerator, time_signature.denominator,
-        //                 time_signature.time))
+            for(var i = 1; i < timeSignatures.length; i++) {
+                var timeSignature = timeSignatures[i];
+                if (timeSignature.getNumerator() != timeSignatures[0].getNumerator() ||
+                        timeSignature.getDenominator() != timeSignatures[0].getDenominator()) {
+                    throw new MultipleTimeSignatureException(
+                        'NoteSequence has at least one time signature change from ' +
+                        timeSignatures[0].getNumerator() + '/' + timeSignatures[0].getDenominator() +
+                        ' to ' + timeSignature.getNumerator() + '/' + timeSignature.getDenominator() +
+                        'at ' + timeSignature.getTime() + ' seconds');
+                }
+            }
 
-        //     # Make it clear that there is only 1 time signature and it starts at the
-        //     # beginning.
-        //     qns.time_signatures[0].time = 0
-        //     del qns.time_signatures[1:]
-        // else:
-        //     time_signature = qns.time_signatures.add()
-        //     time_signature.numerator = 4
-        //     time_signature.denominator = 4
-        //     time_signature.time = 0
+            // Make it clear that there is only 1 time signature and it starts at the
+            // beginning.
+            qns.getTimeSignaturesList()[0].setTime(0);
+            qns.setTimeSignaturesList([qns.getTimeSignaturesList()[0]]);
+        } else {
+            var timeSignature = new music_pb.NoteSequence.TimeSignature();
+            timeSignature.setNumerator(4);
+            timeSignature.setDenominator(4);
+            timeSignature.setTime(0);
+            qns.addTimeSignatures(timeSignature);
+        }
 
-        // if not _is_power_of_2(qns.time_signatures[0].denominator):
-        //     raise BadTimeSignatureException(
-        //         'Denominator is not a power of 2. Time signature: %d/%d' %
-        //         (qns.time_signatures[0].numerator, qns.time_signatures[0].denominator))
+        var firstTS = qns.getTimeSignaturesList()[0];
+        if(!this.isPowerOf2(firstTS.getDenominator())) {
+            throw new BadTimeSignatureException(
+                'Denominator is not a power of 2. Time signature: ' +
+                firstTS.getNumerator() + '/' + firstTS.getDenominator());
+        }
 
-        // if qns.time_signatures[0].numerator == 0:
-        //     raise BadTimeSignatureException(
-        //         'Numerator is 0. Time signature: %d/%d' %
-        //         (qns.time_signatures[0].numerator, qns.time_signatures[0].denominator))
+        if(firstTS.getNumerator() == 0) {
+            throw new BadTimeSignatureException(
+                'Numerator is 0. Time signature: ' +
+                firstTS.getNumerator() + '/' + firstTS.getDenominator());
+        }
 
-        // if qns.tempos:
+        if(qns.getTemposList().length > 0) {
         //     tempos = sorted(qns.tempos, key=lambda t: t.time)
         //     # There is an implicit 120.0 qpm tempo at 0 time. So if the first tempo is
         //     # something other that 120.0 and it's at a time other than 0, that's an
@@ -118,17 +215,20 @@ export class Sequences {
         //     # Make it clear that there is only 1 tempo and it starts at the beginning.
         //     qns.tempos[0].time = 0
         //     del qns.tempos[1:]
-        // else:
-        //     tempo = qns.tempos.add()
-        //     tempo.qpm = constants.DEFAULT_QUARTERS_PER_MINUTE
-        //     tempo.time = 0
+        } else {
+            var tempo = new music_pb.NoteSequence.Tempo();
+            tempo.setQpm(constants.DEFAULT_QUARTERS_PER_MINUTE);
+            tempo.setTime(0);
+            qns.addTempos(tempo);
+        }
 
-        // # Compute quantization steps per second.
-        // steps_per_second = steps_per_quarter_to_steps_per_second(
-        //     steps_per_quarter, qns.tempos[0].qpm)
+        // Compute quantization steps per second.
+        var stepsPerSecond = this.stepsPerQuarterToStepsPerSecond(
+            stepsPerQuarter, qns.getTemposList()[0].getQpm());
 
-        // qns.total_quantized_steps = quantize_to_step(qns.total_time, steps_per_second)
-        // _quantize_notes(qns, steps_per_second)
+        qns.setTotalQuantizedSteps(this.quantizeToStep(
+            qns.getTotalTime(), stepsPerSecond));
+        this.quantizeNotes(qns, stepsPerSecond);
 
         // return qns
         return qns
--- magenta/js/src/sequences_test.ts
+++ magenta/js/src/sequences_test.ts
@@ -22,6 +22,23 @@ import {Sequences} from './sequences';
 
 const STEPS_PER_QUARTER = 4;
 
+function createTestNS() {
+    var ns = new music_pb.NoteSequence();
+
+    var tempo = new music_pb.NoteSequence.Tempo();
+    tempo.setQpm(60);
+    tempo.setTime(0);
+    ns.addTempos(tempo);
+
+    var ts = new music_pb.NoteSequence.TimeSignature();
+    ts.setTime(0);
+    ts.setNumerator(4);
+    ts.setDenominator(4);
+    ns.addTimeSignatures(ts);
+
+    return ns;
+}
+
 function addTrackToSequence(ns, instrument, notes) {
     for (var noteParams of notes) {
         var note = new music_pb.NoteSequence.Note();
@@ -88,18 +105,18 @@ function addQuantizedControlStepsToSequence(ns, quantizedSteps) {
 }
 
 test("Quantize NoteSequence", (t:test.Test) => {
-    var ns = new music_pb.NoteSequence();
+    var ns = createTestNS();
 
     addTrackToSequence(
         ns, 0,
         [[12, 100, 0.01, 10.0], [11, 55, 0.22, 0.50], [40, 45, 2.50, 3.50],
         [55, 120, 4.0, 4.01], [52, 99, 4.75, 5.0]]);
-    addChordsToSequence(
-        ns,
-        [['B7', 0.22], ['Em9', 4.0]]);
-    addControlChangesToSequence(
-        ns, 0,
-        [[2.0, 64, 127], [4.0, 64, 0]]);
+    // addChordsToSequence(
+    //     ns,
+    //     [['B7', 0.22], ['Em9', 4.0]]);
+    // addControlChangesToSequence(
+    //     ns, 0,
+    //     [[2.0, 64, 127], [4.0, 64, 0]]);
 
     var expectedQuantizedSequence = ns.clone();
     expectedQuantizedSequence.setQuantizationInfo(
@@ -109,14 +126,14 @@ test("Quantize NoteSequence", (t:test.Test) => {
     addQuantizedStepsToSequence(
         expectedQuantizedSequence,
         [[0, 40], [1, 2], [10, 14], [16, 17], [19, 20]]);
-    addQuantizedChordStepsToSequence(
-        expectedQuantizedSequence, [1, 16]);
-    addQuantizedControlStepsToSequence(
-        expectedQuantizedSequence, [8, 16]);
+    // addQuantizedChordStepsToSequence(
+    //     expectedQuantizedSequence, [1, 16]);
+    // addQuantizedControlStepsToSequence(
+    //     expectedQuantizedSequence, [8, 16]);
 
     var qns = Sequences.quantizeNoteSequence(ns, STEPS_PER_QUARTER);
 
-    t.deepEqual(expectedQuantizedSequence.toObject(), qns.toObject());
+    t.deepEqual(qns.toObject(), expectedQuantizedSequence.toObject());
 
     t.end();
 });
