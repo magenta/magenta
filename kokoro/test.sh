785c438b39d554b850877c5dc809ddd6b6fd1706
Ian Simon <iansimon@users.noreply.github.com>
2018-03-12 10:23:16 -0700

    temporarily restrict tornado version in kokoro (#1101)

--- kokoro/test.sh
+++ kokoro/test.sh
@@ -5,8 +5,10 @@ set -e
 # Display commands being run.
 set -x
 
+# TODO(iansimon): Unrestrict tornado version and figure out how to upgrade to
+# python 2.7.9 in kokoro.
 eval "${PIP_COMMAND} install --upgrade tensorflow scipy matplotlib \
-  intervaltree bokeh IPython librosa mir_eval"
+  intervaltree bokeh IPython librosa mir_eval 'tornado<5.0'"
 
 bazel test \
   --keep_going \
