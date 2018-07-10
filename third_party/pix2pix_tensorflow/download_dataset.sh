b46b78a79305d219e66eef5424d9e60a243302e2
Damien Henry <damienhenry@damienhenry.c.googlers.com>
2018-07-10 13:52:08 +0000

    remove third party folder

--- third_party/pix2pix_tensorflow/download_dataset.sh
+++
@@ -1,9 +0,0 @@
-mkdir datasets
-FILE=$1
-URL=https://people.eecs.berkeley.edu/~tinghuiz/projects/pix2pix/datasets/$FILE.tar.gz
-TAR_FILE=./datasets/$FILE.tar.gz
-TARGET_DIR=./datasets/$FILE/
-wget -N $URL -O $TAR_FILE
-mkdir $TARGET_DIR
-tar -zxvf $TAR_FILE -C ./datasets/
-rm $TAR_FILE
