81c87c55f1b029438e06e66ea163f388de7169a9
Damien Henry <dh@dh7.net>
2018-07-27 08:07:49 +0200

    add a video folder and some scripts to generate videos using pix2pix recursively

--- WORKSPACE
+++ WORKSPACE
@@ -38,3 +38,11 @@ new_http_archive(
     strip_prefix = "mido-1.2.6/mido",
     url = "https://github.com/olemb/mido/archive/1.2.6.tar.gz",
 )
+
+new_http_archive(
+    name = "pix2pix_tensorflow",
+    build_file = "pix2pix_tensorflow.BUILD",
+    sha256 = "d00e0c8d65b4e3b0b61f5655976eaed0ea5b98b4aba1f8dea96809ec995b7b0e",
+    strip_prefix = "pix2pix-tensorflow-0.1/",
+    url = "https://github.com/dh7/pix2pix-tensorflow/archive/0.1.tar.gz",
+)
---
+++ magenta/video/tools/create_mp4.sh
@@ -0,0 +1,31 @@
+# Copyright 2017 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# this script need to be called from the root of tf-toolbox
+echo "!!! this script need to be called from the root of tf-toolbox"
+
+
+if [ "$#" -ne 2 ]
+then
+    echo "arg 1 is a path/shema that contains images to be converted"
+    echo "  for instance: myfolder/prefix_%04d_sufix.png"
+    echo "  will convert all file from"
+    echo "    myfolder/prefix_0001_sufix.png to"
+    echo "    myfolder/prefix_9999_sufix.png"
+    echo "arg 2 is the video name, for instance video.mp4"
+else
+    ffmpeg -f image2 -i $1 $2
+
+    echo "to add music: ffmpeg -i video.mp4 -i audio.mp3 -codec copy -shortest output.mp4"
+fi
