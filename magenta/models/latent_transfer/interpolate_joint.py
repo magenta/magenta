586d65f7322866c4694d9f1cac17f90c067f2c4f
Yingtao Tian <alantian@users.noreply.github.com>
2018-08-24 15:28:58 -0700

    Check-in Latent Transfer Model (#1270)
    * Intiail check-in of latent transfer model.
    
    * Add joint model after checking into Magenta core.
    
    * Change import
    
    * use tf.{app,flags,logging} directly.
    
    * add BUILD
    
    * add license
    
    * remove visibility

---
+++ magenta/models/latent_transfer/interpolate_joint.py
@@ -0,0 +1,217 @@
+# Copyright 2018 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Produce interpolation in the joint model trained by `train_joint.py`.
+
+This script produces interpolation on one side of the joint model as a series of
+images, as well as in other side of the model through paralleling,
+image-by-image transformation.
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import importlib
+from os.path import join
+
+import numpy as np
+import tensorflow as tf
+
+from magenta.models.latent_transfer import common
+from magenta.models.latent_transfer import common_joint
+from magenta.models.latent_transfer import model_joint
+
+FLAGS = tf.flags.FLAGS
+
+tf.flags.DEFINE_string('config', 'transfer_A_unconditional_mnist_to_mnist',
+                       'The name of the model config to use.')
+tf.flags.DEFINE_string('exp_uid_A', '_exp_0', 'exp_uid for data_A')
+tf.flags.DEFINE_string('exp_uid_B', '_exp_1', 'exp_uid for data_B')
+tf.flags.DEFINE_string('exp_uid', '_exp_0',
+                       'String to append to config for filenames/directories.')
+tf.flags.DEFINE_integer('n_iters', 100000, 'Number of iterations.')
+tf.flags.DEFINE_integer('n_iters_per_save', 5000, 'Iterations per a save.')
+tf.flags.DEFINE_integer('n_iters_per_eval', 5000,
+                        'Iterations per a evaluation.')
+tf.flags.DEFINE_integer('random_seed', 19260817, 'Random seed')
+tf.flags.DEFINE_string('exp_uid_classifier', '_exp_0', 'exp_uid for classifier')
+
+# For Overriding configs
+tf.flags.DEFINE_integer('n_latent', 64, '')
+tf.flags.DEFINE_integer('n_latent_shared', 2, '')
+tf.flags.DEFINE_float('prior_loss_beta_A', 0.01, '')
+tf.flags.DEFINE_float('prior_loss_beta_B', 0.01, '')
+tf.flags.DEFINE_float('prior_loss_align_beta', 0.0, '')
+tf.flags.DEFINE_float('mean_recons_A_align_beta', 0.0, '')
+tf.flags.DEFINE_float('mean_recons_B_align_beta', 0.0, '')
+tf.flags.DEFINE_float('mean_recons_A_to_B_align_beta', 0.0, '')
+tf.flags.DEFINE_float('mean_recons_B_to_A_align_beta', 0.0, '')
+tf.flags.DEFINE_integer('pairing_number', 1024, '')
+
+# For controling interpolation
+tf.flags.DEFINE_integer('load_ckpt_iter', 0, '')
+tf.flags.DEFINE_string('interpolate_labels', '',
+                       'a `,` separated list of 0-indexed labels.')
+tf.flags.DEFINE_integer('nb_images_between_labels', 1, '')
+
+
+def load_config(config_name):
+  return importlib.import_module('configs.%s' % config_name).config
+
+
+def main(unused_argv):
+  # pylint:disable=unused-variable
+  # Reason:
+  #   This training script relys on many programmatical call to function and
+  #   access to variables. Pylint cannot infer this case so it emits false alarm
+  #   of unused-variable if we do not disable this warning.
+
+  # pylint:disable=invalid-name
+  # Reason:
+  #   Following variables have their name consider to be invalid by pylint so
+  #   we disable the warning.
+  #   - Variable that in its name has A or B indictating their belonging of
+  #     one side of data.
+  del unused_argv
+
+  # Load main config
+  config_name = FLAGS.config
+  config = load_config(config_name)
+
+  config_name_A = config['config_A']
+  config_name_B = config['config_B']
+  config_name_classifier_A = config['config_classifier_A']
+  config_name_classifier_B = config['config_classifier_B']
+
+  # Load dataset
+  dataset_A = common_joint.load_dataset(config_name_A, FLAGS.exp_uid_A)
+  (dataset_blob_A, train_data_A, train_label_A, train_mu_A, train_sigma_A,
+   index_grouped_by_label_A) = dataset_A
+  dataset_B = common_joint.load_dataset(config_name_B, FLAGS.exp_uid_B)
+  (dataset_blob_B, train_data_B, train_label_B, train_mu_B, train_sigma_B,
+   index_grouped_by_label_B) = dataset_B
+
+  # Prepare directories
+  dirs = common_joint.prepare_dirs('joint', config_name, FLAGS.exp_uid)
+  save_dir, sample_dir = dirs
+
+  # Set random seed
+  np.random.seed(FLAGS.random_seed)
+  tf.set_random_seed(FLAGS.random_seed)
+
+  # Real Training.
+  tf.reset_default_graph()
+  sess = tf.Session()
+
+  # Load model's architecture (= build)
+  one_side_helper_A = common_joint.OneSideHelper(config_name_A, FLAGS.exp_uid_A,
+                                                 config_name_classifier_A,
+                                                 FLAGS.exp_uid_classifier)
+  one_side_helper_B = common_joint.OneSideHelper(config_name_B, FLAGS.exp_uid_B,
+                                                 config_name_classifier_B,
+                                                 FLAGS.exp_uid_classifier)
+  m = common_joint.load_model(model_joint.Model, config_name, FLAGS.exp_uid)
+
+  # Initialize and restore
+  sess.run(tf.global_variables_initializer())
+
+  one_side_helper_A.restore(dataset_blob_A)
+  one_side_helper_B.restore(dataset_blob_B)
+
+  # Restore from ckpt
+  config_name = FLAGS.config
+  model_uid = common.get_model_uid(config_name, FLAGS.exp_uid)
+  save_name = join(save_dir,
+                   'transfer_%s_%d.ckpt' % (model_uid, FLAGS.load_ckpt_iter))
+  m.vae_saver.restore(sess, save_name)
+
+  # prepare intepolate dir
+  intepolate_dir = join(sample_dir, 'interpolate_sample',
+                        '%010d' % FLAGS.load_ckpt_iter)
+  tf.gfile.MakeDirs(intepolate_dir)
+
+  # things
+  interpolate_labels = [int(_) for _ in FLAGS.interpolate_labels.split(',')]
+  nb_images_between_labels = FLAGS.nb_images_between_labels
+
+  index_list_A = []
+  last_pos = [0] * 10
+  for label in interpolate_labels:
+    index_list_A.append(index_grouped_by_label_A[label][last_pos[label]])
+    last_pos[label] += 1
+
+  index_list_B = []
+  last_pos = [-1] * 10
+  for label in interpolate_labels:
+    index_list_B.append(index_grouped_by_label_B[label][last_pos[label]])
+    last_pos[label] -= 1
+
+  z_A = []
+  z_A.append(train_mu_A[index_list_A[0]])
+  for i_label in range(1, len(interpolate_labels)):
+    last_z_A = z_A[-1]
+    this_z_A = train_mu_A[index_list_A[i_label]]
+    for j in range(1, nb_images_between_labels + 1):
+      z_A.append(last_z_A +
+                 (this_z_A - last_z_A) * (float(j) / nb_images_between_labels))
+  z_B = []
+  z_B.append(train_mu_B[index_list_B[0]])
+  for i_label in range(1, len(interpolate_labels)):
+    last_z_B = z_B[-1]
+    this_z_B = train_mu_B[index_list_B[i_label]]
+    for j in range(1, nb_images_between_labels + 1):
+      z_B.append(last_z_B +
+                 (this_z_B - last_z_B) * (float(j) / nb_images_between_labels))
+  z_B_tr = []
+  for this_z_A in z_A:
+    this_z_B_tr = sess.run(m.x_A_to_B_direct, {m.x_A: np.array([this_z_A])})
+    z_B_tr.append(this_z_B_tr[0])
+
+  # Generate data domain instances and save.
+  z_A = np.array(z_A)
+  x_A = one_side_helper_A.m_helper.decode(z_A)
+  x_A = common.post_proc(x_A, one_side_helper_A.m_helper.config)
+  batched_x_A = common.batch_image(
+      x_A,
+      max_images=len(x_A),
+      rows=len(x_A),
+      cols=1,
+  )
+  common.save_image(batched_x_A, join(intepolate_dir, 'x_A.png'))
+
+  z_B = np.array(z_B)
+  x_B = one_side_helper_B.m_helper.decode(z_B)
+  x_B = common.post_proc(x_B, one_side_helper_B.m_helper.config)
+  batched_x_B = common.batch_image(
+      x_B,
+      max_images=len(x_B),
+      rows=len(x_B),
+      cols=1,
+  )
+  common.save_image(batched_x_B, join(intepolate_dir, 'x_B.png'))
+
+  z_B_tr = np.array(z_B_tr)
+  x_B_tr = one_side_helper_B.m_helper.decode(z_B_tr)
+  x_B_tr = common.post_proc(x_B_tr, one_side_helper_B.m_helper.config)
+  batched_x_B_tr = common.batch_image(
+      x_B_tr,
+      max_images=len(x_B_tr),
+      rows=len(x_B_tr),
+      cols=1,
+  )
+  common.save_image(batched_x_B_tr, join(intepolate_dir, 'x_B_tr.png'))
+
+
+if __name__ == '__main__':
+  tf.app.run(main)
---
+++ magenta/models/latent_transfer/sample_wavegan.py
@@ -0,0 +1,180 @@
+# Copyright 2018 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Sample from pre-trained WaveGAN model.
+
+This script provides sampling from pre-trained WaveGAN model that is done
+through the original author's code (https://github.com/chrisdonahue/wavegan).
+The main purpose is to help manually check the quality of WaveGAN model.
+"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from operator import itemgetter
+from os.path import join
+
+import numpy as np
+from scipy.io import wavfile
+import tensorflow as tf
+from tqdm import tqdm
+
+FLAGS = tf.flags.FLAGS
+
+tf.flags.DEFINE_integer('total_per_label', '7000',
+                        'Minimal # samples per label')
+tf.flags.DEFINE_integer('top_per_label', '1700', '# of top samples per label')
+tf.flags.DEFINE_string('gen_ckpt_dir', '',
+                       'The directory to WaveGAN generator\'s ckpt.')
+tf.flags.DEFINE_string(
+    'inception_ckpt_dir', '',
+    'The directory to WaveGAN inception (classifier)\'s ckpt.')
+tf.flags.DEFINE_string('latent_dir', '',
+                       'The directory to WaveGAN\'s latent space.')
+
+
+def main(unused_argv):
+
+  # pylint:disable=invalid-name
+  # Reason:
+  #   Following variables have their name consider to be invalid by pylint so
+  #   we disable the warning.
+  #   - Variable that is class
+
+  del unused_argv
+
+  use_gaussian_pretrained_model = FLAGS.use_gaussian_pretrained_model
+
+  gen_ckpt_dir = FLAGS.gen_ckpt_dir
+  inception_ckpt_dir = FLAGS.inception_ckpt_dir
+
+  # TF init
+  tf.reset_default_graph()
+  # - generative model
+  graph_gan = tf.Graph()
+  with graph_gan.as_default():
+    sess_gan = tf.Session(graph=graph_gan)
+    if use_gaussian_pretrained_model:
+      saver_gan = tf.train.import_meta_graph(
+          join(gen_ckpt_dir, '..', 'infer', 'infer.meta'))
+      saver_gan.restore(sess_gan, join(gen_ckpt_dir, 'model.ckpt'))
+    else:
+      saver_gan = tf.train.import_meta_graph(join(gen_ckpt_dir, 'infer.meta'))
+      saver_gan.restore(sess_gan, join(gen_ckpt_dir, 'model.ckpt'))
+  # - classifier (inception)
+  graph_class = tf.Graph()
+  with graph_class.as_default():
+    sess_class = tf.Session(graph=graph_class)
+    saver_class = tf.train.import_meta_graph(
+        join(inception_ckpt_dir, 'infer.meta'))
+    saver_class.restore(sess_class, join(inception_ckpt_dir, 'best_acc-103005'))
+
+  # Generate: Tensor symbols
+  z = graph_gan.get_tensor_by_name('z:0')
+  G_z = graph_gan.get_tensor_by_name('G_z:0')[:, :, 0]
+  # G_z_spec = graph_gan.get_tensor_by_name('G_z_spec:0')
+  # Classification: Tensor symbols
+  x = graph_class.get_tensor_by_name('x:0')
+  scores = graph_class.get_tensor_by_name('scores:0')
+
+  # Sample something AND classify them
+
+  output_dir = FLAGS.latent_dir
+
+  tf.gfile.MakeDirs(output_dir)
+
+  np.random.seed(19260817)
+  total_per_label = FLAGS.total_per_label
+  top_per_label = FLAGS.top_per_label
+  group_by_label = [[] for _ in range(10)]
+  batch_size = 200
+  hidden_dim = 100
+
+  with tqdm(desc='min label count', unit=' #', total=total_per_label) as pbar:
+    label_count = [0] * 10
+    last_min_label_count = 0
+    while True:
+      min_label_count = min(label_count)
+      pbar.update(min_label_count - last_min_label_count)
+      last_min_label_count = min_label_count
+
+      if use_gaussian_pretrained_model:
+        _z = np.random.randn(batch_size, hidden_dim)
+      else:
+        _z = (np.random.rand(batch_size, hidden_dim) * 2.) - 1.
+      # _G_z, _G_z_spec = sess_gan.run([G_z, G_z_spec], {z: _z})
+      _G_z = sess_gan.run(G_z, {z: _z})
+      _x = _G_z
+      _scores = sess_class.run(scores, {x: _x})
+      _max_scores = np.max(_scores, axis=1)
+      _labels = np.argmax(_scores, axis=1)
+      for i in range(batch_size):
+        label = _labels[i]
+
+        group_by_label[label].append((_max_scores[i], (_z[i], _G_z[i])))
+        label_count[label] += 1
+
+        if len(group_by_label[label]) >= top_per_label * 2:
+          # remove unneeded tails
+          group_by_label[label].sort(key=itemgetter(0), reverse=True)
+          group_by_label[label] = group_by_label[label][:top_per_label]
+
+      if last_min_label_count >= total_per_label:
+        break
+
+  for label in range(10):
+    group_by_label[label].sort(key=itemgetter(0), reverse=True)
+    group_by_label[label] = group_by_label[label][:top_per_label]
+
+  # output a few samples as image
+  image_output_dir = join(output_dir, 'sample_iamge')
+  tf.gfile.MakeDirs(image_output_dir)
+
+  for label in range(10):
+    group_by_label[label].sort(key=itemgetter(0), reverse=True)
+    index = 0
+    for confidence, (
+        _,
+        this_G_z,
+    ) in group_by_label[label][:10]:
+      output_basename = 'predlabel=%d_index=%02d_confidence=%.6f' % (
+          label, index, confidence)
+      wavfile.write(
+          filename=join(image_output_dir, output_basename + '_sound.wav'),
+          rate=16000,
+          data=this_G_z)
+
+  # Make Numpy arrays and save everything as an npz file
+  array_label, array_z, array_G_z = [], [], []
+  for label in range(10):
+    for _, blob in group_by_label[label]:
+      this_z, this_G_z = blob[:2]
+      array_label.append(label)
+      array_z.append(this_z)
+      array_G_z.append(this_G_z)
+  array_label = np.array(array_label, dtype='i')
+  array_z = np.array(array_z)
+  array_G_z = np.array(array_G_z)
+
+  np.savez(
+      join(output_dir, 'data_train.npz'),
+      label=array_label,
+      z=array_z,
+      G_z=array_G_z,
+  )
+
+  # pylint:enable=invalid-name
+
+
+if __name__ == '__main__':
+  tf.app.run(main)
