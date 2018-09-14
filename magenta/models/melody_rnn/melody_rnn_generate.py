b888bf4962d53263c1234431b98c62189ae7b8a5
Adam Roberts <adarob@google.com>
2018-09-13 20:51:18 -0700

    reviewer comments

--- magenta/models/melody_rnn/melody_rnn_generate.py
+++ magenta/models/melody_rnn/melody_rnn_generate.py
@@ -13,8 +13,6 @@
 # limitations under the License.
 """Generate melodies from a trained checkpoint of a melody RNN model."""
 
-# TODO(adarob): Add support for models with conditioning.
-
 import ast
 import os
 import time
--- magenta/models/music_vae/music_vae_generate.py
+++ magenta/models/music_vae/music_vae_generate.py
@@ -11,7 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""MusicVAE generationg script."""
+"""MusicVAE generation script."""
+
+# TODO(adarob): Add support for models with conditioning.
 
 from __future__ import absolute_import
 from __future__ import division
