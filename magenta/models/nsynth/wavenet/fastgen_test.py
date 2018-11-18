8de0620e64a5e0d2786e223b001cb3ac62bcffe2
jesseengel <jesse.engel@gmail.com>
2018-11-18 12:14:16 -0800

    added tests for loading nsynth config

--- magenta/models/nsynth/wavenet/fastgen_test.py
+++ magenta/models/nsynth/wavenet/fastgen_test.py
@@ -14,14 +14,28 @@ import fastgen
 class FastegenTest(parameterized.TestCase, tf.test.TestCase):
 
   @parameterized.parameters(
-      {'shape': [3, 10, 10, 3]},
-      {'shape': [4, 5, 5, 1]})
-  def testPixelNorm(self, shape, eps=1e-8):
-    images_np = np.random.randn(*shape)
-    images_tf = tf.convert_to_tensor(images_np)
+      {'batch_size': 1},
+      {'batch_size': 10})
+  def testLoadFastgenNsynth(self, batch_size):
+    net = fastgen.load_fastgen_nsynth(batch_size=batch_size)
     with self.test_session() as sess:
-      r_tf = sess.run(images_tf)
-      self.assertEqual(images_np.shape, r_tf.shape)
+      sess.run(net['init_ops'])
+      self.assertEqual(net['X'].shape, (batch_size, 1))
+      self.assertEqual(net['encoding'].shape, (batch_size, 16))
+      self.assertEqual(net['predictions'].shape, (batch_size, 256))
+
+  @parameterized.parameters(
+      {'batch_size': 1, 'sample_length': 1024 * 10},
+      {'batch_size': 10, 'sample_length': 1024 * 10},
+      {'batch_size': 10, 'sample_length': 1024 * 20},
+  )
+  def testLoadNsynth(self, batch_size, sample_length):
+    net = fastgen.load_nsynth(batch_size=batch_size, sample_length=sample_length)
+    encodings_length = int(sample_length/512)
+    with self.test_session() as sess:
+      self.assertEqual(net['X'].shape, (batch_size, sample_length))
+      self.assertEqual(net['encoding'].shape, (batch_size, encodings_length, 16))
+      self.assertEqual(net['predictions'].shape, (batch_size * sample_length, 256))
 
 
 if __name__ == '__main__':
