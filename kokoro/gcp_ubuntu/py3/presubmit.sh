05ef3cc3b32143691f77c8f5637d037923ff6325
Adam Roberts <adarob@google.com>
2018-12-06 13:26:09 -0800

    kokoro

--- kokoro/gcp_ubuntu/py3/presubmit.sh
+++ kokoro/gcp_ubuntu/py3/presubmit.sh
@@ -10,6 +10,6 @@ PY3_PATH="$(which python3.5)"
 
 cd github/magenta
 
-sudo apt-get install build-essential libasound2-dev libjack-dev
+sudo apt-get -y install build-essential libasound2-dev libjack-dev
 $PY3_PATH setup.py test --adopts="--pylint -m pylint"
 $PY3_PATH setup.py test
