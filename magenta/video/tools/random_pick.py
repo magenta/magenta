85a7bf67f0ca2a974057e37cc66b59b5b2224bf0
Adam Roberts <adarob@google.com>
2019-01-18 13:27:14 -0800

    Remove license from files in third_party/py/magenta since they will be automatically added by copybara.
    PiperOrigin-RevId: 229983538

--- magenta/video/tools/random_pick.py
+++ magenta/video/tools/random_pick.py
@@ -12,19 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Copyright 2017 Google Inc. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 """This tools pick some frames randomly from a folder to an other.
 
 Only useful if used with the --limit flag unless it will copy the whole folder
