7ba2fae4d030f146ab7f097526154a3c5ce962c0
Curtis Hawthorne <fjord@google.com>
2019-01-22 11:30:10 -0800

    Project import generated by Copybara.
    PiperOrigin-RevId: 230378375

---
+++ ci-install.sh
@@ -0,0 +1,26 @@
+# Copyright 2018 The Magenta Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#!/bin/bash
+
+##
+# Steps needed to set up CI environment.
+##
+
+set -e
+set -x
+
+sudo apt-get -y install build-essential libasound2-dev libjack-dev libav-tools
+pip install pylint
+python setup.py install
