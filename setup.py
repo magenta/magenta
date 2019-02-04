f2bd595a1c001b41ca494e1b73b4fdd461c94a39
Curtis Hawthorne <fjord@google.com>
2019-02-04 14:31:30 -0800

    remove pylint/pytest from setup.py

--- setup.py
+++ setup.py
@@ -152,6 +152,4 @@ setup(
     package_data={
         'magenta': ['models/image_stylization/evaluation_images/*.jpg'],
     },
-    setup_requires=['pytest-runner', 'pytest-pylint'],
-    tests_require=['pytest', 'pylint'],
 )
