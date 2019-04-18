4c57e7291d8dd089f347ecf3f4fb20bffcdbdbb8
Sam Brotherton <sbrother@gmail.com>
2019-04-17 21:27:05 -0600

    Lock tensorflow to working 1.13.1 version

--- setup.py
+++ setup.py
@@ -62,9 +62,9 @@ REQUIRED_PACKAGES = [
 ]
 
 if gpu_mode:
-  REQUIRED_PACKAGES.append('tensorflow-gpu >= 1.12.0')
+  REQUIRED_PACKAGES.append('tensorflow-gpu == 1.13.1')
 else:
-  REQUIRED_PACKAGES.append('tensorflow >= 1.12.0')
+  REQUIRED_PACKAGES.append('tensorflow == 1.13.1')
 
 # pylint:disable=line-too-long
 CONSOLE_SCRIPTS = [
