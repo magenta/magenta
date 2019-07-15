b0c89b086c74248096c265541b6e84b2af61e9ff
Adam Roberts <adarob@google.com>
2019-07-15 15:13:28 -0700

    Reorder definitions to make linter happy (in Travis).
    PiperOrigin-RevId: 258246956

--- magenta/pipelines/drum_pipelines.py
+++ magenta/pipelines/drum_pipelines.py
@@ -24,33 +24,6 @@ from magenta.protobuf import music_pb2
 import tensorflow as tf
 
 
-class DrumsExtractor(pipeline.Pipeline):
-  """Extracts drum tracks from a quantized NoteSequence."""
-
-  def __init__(self, min_bars=7, max_steps=512, gap_bars=1.0, name=None):
-    super(DrumsExtractor, self).__init__(
-        input_type=music_pb2.NoteSequence,
-        output_type=drums_lib.DrumTrack,
-        name=name)
-    self._min_bars = min_bars
-    self._max_steps = max_steps
-    self._gap_bars = gap_bars
-
-  def transform(self, quantized_sequence):
-    try:
-      drum_tracks, stats = extract_drum_tracks(
-          quantized_sequence,
-          min_bars=self._min_bars,
-          max_steps_truncate=self._max_steps,
-          gap_bars=self._gap_bars)
-    except events_lib.NonIntegerStepsPerBarError as detail:
-      tf.logging.warning('Skipped sequence: %s', detail)
-      drum_tracks = []
-      stats = [statistics.Counter('non_integer_steps_per_bar', 1)]
-    self._set_stats(stats)
-    return drum_tracks
-
-
 def extract_drum_tracks(quantized_sequence,
                         search_start_step=0,
                         min_bars=7,
@@ -160,3 +133,30 @@ def extract_drum_tracks(quantized_sequence,
     drum_tracks.append(drum_track)
 
   return drum_tracks, stats.values()
+
+
+class DrumsExtractor(pipeline.Pipeline):
+  """Extracts drum tracks from a quantized NoteSequence."""
+
+  def __init__(self, min_bars=7, max_steps=512, gap_bars=1.0, name=None):
+    super(DrumsExtractor, self).__init__(
+        input_type=music_pb2.NoteSequence,
+        output_type=drums_lib.DrumTrack,
+        name=name)
+    self._min_bars = min_bars
+    self._max_steps = max_steps
+    self._gap_bars = gap_bars
+
+  def transform(self, quantized_sequence):
+    try:
+      drum_tracks, stats = extract_drum_tracks(
+          quantized_sequence,
+          min_bars=self._min_bars,
+          max_steps_truncate=self._max_steps,
+          gap_bars=self._gap_bars)
+    except events_lib.NonIntegerStepsPerBarError as detail:
+      tf.logging.warning('Skipped sequence: %s', detail)
+      drum_tracks = []
+      stats = [statistics.Counter('non_integer_steps_per_bar', 1)]
+    self._set_stats(stats)
+    return drum_tracks
