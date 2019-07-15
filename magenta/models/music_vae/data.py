40e784dcc5cee02a9595e36fdde0a8c0296abf6a
Adam Roberts <adarob@google.com>
2019-07-15 14:32:56 -0700

    Revert FULL_DRUM_PITCH_CLASSES to values from before DEFAULT_DRUM_TYPE_PITCHES was modified.
    PiperOrigin-RevId: 258238834

--- magenta/models/music_vae/data.py
+++ magenta/models/music_vae/data.py
@@ -52,7 +52,12 @@ ELECTRIC_BASS_PROGRAM = 33
 REDUCED_DRUM_PITCH_CLASSES = drums_encoder_decoder.DEFAULT_DRUM_TYPE_PITCHES
 # 61 classes: full General MIDI set
 FULL_DRUM_PITCH_CLASSES = [
-    [p] for c in drums_encoder_decoder.DEFAULT_DRUM_TYPE_PITCHES for p in c]
+    [p] for p in  # pylint:disable=g-complex-comprehension
+    [36, 35, 38, 27, 28, 31, 32, 33, 34, 37, 39, 40, 56, 65, 66, 75, 85, 42, 44,
+     54, 68, 69, 70, 71, 73, 78, 80, 46, 67, 72, 74, 79, 81, 45, 29, 41, 61, 64,
+     84, 48, 47, 60, 63, 77, 86, 87, 50, 30, 43, 62, 76, 83, 49, 55, 57, 58, 51,
+     52, 53, 59, 82]
+]
 ROLAND_DRUM_PITCH_CLASSES = [
     # kick drum
     [36],
