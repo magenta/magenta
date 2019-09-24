286f165b6cb9cfa98a69fb91c62b714da2f5061e
Ondrej Cifka <cifkao@users.noreply.github.com>
2019-09-24 11:36:40 +0200

    Fix TF version

--- setup.py
+++ setup.py
@@ -70,10 +70,10 @@ REQUIRED_PACKAGES = [
 
 # pylint: disable=undefined-variable
 if gpu_mode:
-  REQUIRED_PACKAGES.append('tensorflow-gpu >= 1.13.0')
+  REQUIRED_PACKAGES.append('tensorflow-gpu >= 1.14.0')
   REQUIRED_PACKAGES.append('magenta.music-gpu == ' + __version__)
 else:
-  REQUIRED_PACKAGES.append('tensorflow >= 1.13.0')
+  REQUIRED_PACKAGES.append('tensorflow >= 1.14.0')
   REQUIRED_PACKAGES.append('magenta.music == ' + __version__)
 # pylint: enable=undefined-variable
 
