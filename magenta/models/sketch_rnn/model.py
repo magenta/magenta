99e5626d2ebd36dff8770b06b10a8545c68cf612
Curtis Hawthorne <fjord@google.com>
2020-03-16 18:24:34 -0700

    Improve python 3 compatibility.
    PiperOrigin-RevId: 301279557

--- magenta/models/sketch_rnn/model.py
+++ magenta/models/sketch_rnn/model.py
@@ -12,12 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Lint as: python2, python3
+# Lint as: python3
 """Sketch-RNN Model."""
-
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
 
 import random
 
--- magenta/models/sketch_rnn/utils.py
+++ magenta/models/sketch_rnn/utils.py
@@ -12,12 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Lint as: python2, python3
+# Lint as: python3
 """SketchRNN data loading and image manipulation utilities."""
-
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
 
 import random
 
