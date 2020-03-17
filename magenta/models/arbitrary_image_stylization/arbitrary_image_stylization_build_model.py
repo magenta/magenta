fcf758d4066051fde06040754b940624696d8b16
Curtis Hawthorne <fjord@google.com>
2020-03-17 10:06:33 -0700

    Improve python 3 compatibility.
    PiperOrigin-RevId: 301396992

--- magenta/models/arbitrary_image_stylization/arbitrary_image_stylization_build_model.py
+++ magenta/models/arbitrary_image_stylization/arbitrary_image_stylization_build_model.py
@@ -15,10 +15,6 @@
 # Lint as: python3
 """Methods for building real-time arbitrary image stylization model."""
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
 from magenta.models.arbitrary_image_stylization import arbitrary_image_stylization_losses as losses
 from magenta.models.arbitrary_image_stylization import nza_model as transformer_model
 from magenta.models.image_stylization import ops
