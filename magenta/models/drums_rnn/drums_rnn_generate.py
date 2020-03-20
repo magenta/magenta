ab80bfdf549bce80369a669d46ee2b8b378346a3
Curtis Hawthorne <fjord@google.com>
2020-03-20 13:16:23 -0700

    Improve python 3 compatibility.
    PiperOrigin-RevId: 302086717

--- magenta/models/drums_rnn/drums_rnn_generate.py
+++ magenta/models/drums_rnn/drums_rnn_generate.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Lint as: python2, python3
+# Lint as: python3
 """Generate drum tracks from a trained checkpoint of a drums RNN model.
 
 Uses flags to define operation.
