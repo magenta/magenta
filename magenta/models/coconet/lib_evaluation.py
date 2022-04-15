8de203a28b7d9dd81b50562fe2512a771d5e2f07
Yilei Yang <yileiyang@google.com>
2022-04-15 11:02:49 -0700

    Remove unused comments related to Python 2 compatibility.
    PiperOrigin-RevId: 442053623

--- magenta/models/coconet/lib_evaluation.py
+++ magenta/models/coconet/lib_evaluation.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Lint as: python3
 """Helpers for evaluating the log likelihood of pianorolls under a model."""
 import time
 
--- magenta/models/image_stylization/image_stylization_finetune.py
+++ magenta/models/image_stylization/image_stylization_finetune.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Lint as: python3
 """Trains an N-styles style transfer model on the cheap.
 
 Training is done by finetuning the instance norm parameters of a pre-trained
