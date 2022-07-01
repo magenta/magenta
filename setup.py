4f84e0f3165fd662730754624bd94a295ce89063
Prasad Jivane <prasad.jivane@walchandsangli.ac.in>
2022-07-02 03:10:23 +0530

    Updated setup file
    Modified

--- setup.py
+++ setup.py
@@ -14,150 +14,150 @@
 
 """A setuptools based setup module for magenta."""
 
-from setuptools import find_packages
+from setuptools import find_packages, __version__
 from setuptools import setup
 
 # Bit of a hack to parse the version string stored in version.py without
 # executing __init__.py, which will end up requiring a bunch of dependencies to
 # execute (e.g., tensorflow, pretty_midi, etc.).
 # Makes the __version__ variable available.
-with open('magenta/version.py') as in_file:
-  exec(in_file.read())  # pylint: disable=exec-used
+with open("magenta/version.py") as in_file:
+    exec(in_file.read())  # pylint: disable=exec-used
 
 REQUIRED_PACKAGES = [
-    'absl-py',
-    'dm-sonnet',
+    "absl-py",
+    "dm-sonnet",
     # tensor2tensor has a dependency on dopamine-rl, which we don't use.
     # pin to a version that doesn't require pygame installation because that
     # has too many external non-python dependencies.
-    'dopamine-rl <= 3.0.1',
-    'imageio',
-    'librosa >= 0.6.2, < 0.8.0',
-    'matplotlib >= 1.5.3',
-    'mido == 1.2.6',
-    'mir_eval >= 0.4',
-    'note-seq',
-    'numba < 0.50',  # temporary fix for librosa import
-    'numpy',
-    'Pillow >= 3.4.2',
-    'pretty_midi >= 0.2.6',
-    'pygtrie >= 2.3',
-    'python-rtmidi >= 1.1, < 1.2',  # 1.2 breaks us
-    'scikit-image',
-    'scipy >= 0.18.1',
-    'six >= 1.12.0',
-    'sk-video',
-    'sox >= 1.3.7',
-    'tensor2tensor',
-    'gym < 0.22.0',  # Temporary fix to avoid problems in tensor2tensor.
-    'tensorflow',
-    'tensorflow-datasets',
-    'tensorflow-probability',
-    'tf_slim',
-    'wheel',
+    "dopamine-rl <= 3.0.1",
+    "imageio",
+    "librosa >= 0.6.2, < 0.8.0",
+    "matplotlib >= 1.5.3",
+    "mido == 1.2.6",
+    "mir_eval >= 0.4",
+    "note-seq",
+    "numba < 0.50",  # temporary fix for librosa import
+    "numpy",
+    "Pillow >= 3.4.2",
+    "pretty_midi >= 0.2.6",
+    "pygtrie >= 2.3",
+    "python-rtmidi >= 1.1, < 1.2",  # 1.2 breaks us
+    "scikit-image",
+    "scipy >= 0.18.1",
+    "six >= 1.12.0",
+    "sk-video",
+    "sox >= 1.3.7",
+    "tensor2tensor",
+    "gym < 0.22.0",  # Temporary fix to avoid problems in tensor2tensor.
+    "tensorflow",
+    "tensorflow-datasets",
+    "tensorflow-probability",
+    "tf_slim",
+    "wheel",
 ]
 
 EXTRAS_REQUIRE = {
-    'beam': [
-        'apache-beam[gcp] >= 2.14.0',
+    "beam": [
+        "apache-beam[gcp] >= 2.14.0",
     ],
-    'onsets_frames_realtime': [
-        'pyaudio',
-        'colorama',
-        'tflite',
+    "onsets_frames_realtime": [
+        "pyaudio",
+        "colorama",
+        "tflite",
     ],
-    'test': [
-        'pylint',
-        'pytest',
-    ]
+    "test": [
+        "pylint",
+        "pytest",
+    ],
 }
 
 # pylint:disable=line-too-long
 CONSOLE_SCRIPTS = [
-    'magenta.interfaces.midi.magenta_midi',
-    'magenta.interfaces.midi.midi_clock',
-    'magenta.models.arbitrary_image_stylization.arbitrary_image_stylization_evaluate',
-    'magenta.models.arbitrary_image_stylization.arbitrary_image_stylization_train',
-    'magenta.models.arbitrary_image_stylization.arbitrary_image_stylization_with_weights',
-    'magenta.models.arbitrary_image_stylization.arbitrary_image_stylization_distill_mobilenet',
-    'magenta.models.drums_rnn.drums_rnn_create_dataset',
-    'magenta.models.drums_rnn.drums_rnn_generate',
-    'magenta.models.drums_rnn.drums_rnn_train',
-    'magenta.models.image_stylization.image_stylization_create_dataset',
-    'magenta.models.image_stylization.image_stylization_evaluate',
-    'magenta.models.image_stylization.image_stylization_finetune',
-    'magenta.models.image_stylization.image_stylization_train',
-    'magenta.models.image_stylization.image_stylization_transform',
-    'magenta.models.improv_rnn.improv_rnn_create_dataset',
-    'magenta.models.improv_rnn.improv_rnn_generate',
-    'magenta.models.improv_rnn.improv_rnn_train',
-    'magenta.models.gansynth.gansynth_train',
-    'magenta.models.gansynth.gansynth_generate',
-    'magenta.models.melody_rnn.melody_rnn_create_dataset',
-    'magenta.models.melody_rnn.melody_rnn_generate',
-    'magenta.models.melody_rnn.melody_rnn_train',
-    'magenta.models.music_vae.music_vae_generate',
-    'magenta.models.music_vae.music_vae_train',
-    'magenta.models.nsynth.wavenet.nsynth_generate',
-    'magenta.models.nsynth.wavenet.nsynth_save_embeddings',
-    'magenta.models.onsets_frames_transcription.onsets_frames_transcription_create_dataset',
-    'magenta.models.onsets_frames_transcription.onsets_frames_transcription_create_dataset_maps',
-    'magenta.models.onsets_frames_transcription.onsets_frames_transcription_create_tfrecords',
-    'magenta.models.onsets_frames_transcription.onsets_frames_transcription_infer',
-    'magenta.models.onsets_frames_transcription.onsets_frames_transcription_train',
-    'magenta.models.onsets_frames_transcription.onsets_frames_transcription_transcribe',
-    'magenta.models.onsets_frames_transcription.realtime.onsets_frames_transcription_realtime',
-    'magenta.models.performance_rnn.performance_rnn_create_dataset',
-    'magenta.models.performance_rnn.performance_rnn_generate',
-    'magenta.models.performance_rnn.performance_rnn_train',
-    'magenta.models.pianoroll_rnn_nade.pianoroll_rnn_nade_create_dataset',
-    'magenta.models.pianoroll_rnn_nade.pianoroll_rnn_nade_generate',
-    'magenta.models.pianoroll_rnn_nade.pianoroll_rnn_nade_train',
-    'magenta.models.polyphony_rnn.polyphony_rnn_create_dataset',
-    'magenta.models.polyphony_rnn.polyphony_rnn_generate',
-    'magenta.models.polyphony_rnn.polyphony_rnn_train',
-    'magenta.models.rl_tuner.rl_tuner_train',
-    'magenta.models.sketch_rnn.sketch_rnn_train',
-    'magenta.scripts.convert_dir_to_note_sequences',
-    'magenta.tensor2tensor.t2t_datagen',
-    'magenta.tensor2tensor.t2t_decoder',
-    'magenta.tensor2tensor.t2t_trainer',
+    "magenta.interfaces.midi.magenta_midi",
+    "magenta.interfaces.midi.midi_clock",
+    "magenta.models.arbitrary_image_stylization.arbitrary_image_stylization_evaluate",
+    "magenta.models.arbitrary_image_stylization.arbitrary_image_stylization_train",
+    "magenta.models.arbitrary_image_stylization.arbitrary_image_stylization_with_weights",
+    "magenta.models.arbitrary_image_stylization.arbitrary_image_stylization_distill_mobilenet",
+    "magenta.models.drums_rnn.drums_rnn_create_dataset",
+    "magenta.models.drums_rnn.drums_rnn_generate",
+    "magenta.models.drums_rnn.drums_rnn_train",
+    "magenta.models.image_stylization.image_stylization_create_dataset",
+    "magenta.models.image_stylization.image_stylization_evaluate",
+    "magenta.models.image_stylization.image_stylization_finetune",
+    "magenta.models.image_stylization.image_stylization_train",
+    "magenta.models.image_stylization.image_stylization_transform",
+    "magenta.models.improv_rnn.improv_rnn_create_dataset",
+    "magenta.models.improv_rnn.improv_rnn_generate",
+    "magenta.models.improv_rnn.improv_rnn_train",
+    "magenta.models.gansynth.gansynth_train",
+    "magenta.models.gansynth.gansynth_generate",
+    "magenta.models.melody_rnn.melody_rnn_create_dataset",
+    "magenta.models.melody_rnn.melody_rnn_generate",
+    "magenta.models.melody_rnn.melody_rnn_train",
+    "magenta.models.music_vae.music_vae_generate",
+    "magenta.models.music_vae.music_vae_train",
+    "magenta.models.nsynth.wavenet.nsynth_generate",
+    "magenta.models.nsynth.wavenet.nsynth_save_embeddings",
+    "magenta.models.onsets_frames_transcription.onsets_frames_transcription_create_dataset",
+    "magenta.models.onsets_frames_transcription.onsets_frames_transcription_create_dataset_maps",
+    "magenta.models.onsets_frames_transcription.onsets_frames_transcription_create_tfrecords",
+    "magenta.models.onsets_frames_transcription.onsets_frames_transcription_infer",
+    "magenta.models.onsets_frames_transcription.onsets_frames_transcription_train",
+    "magenta.models.onsets_frames_transcription.onsets_frames_transcription_transcribe",
+    "magenta.models.onsets_frames_transcription.realtime.onsets_frames_transcription_realtime",
+    "magenta.models.performance_rnn.performance_rnn_create_dataset",
+    "magenta.models.performance_rnn.performance_rnn_generate",
+    "magenta.models.performance_rnn.performance_rnn_train",
+    "magenta.models.pianoroll_rnn_nade.pianoroll_rnn_nade_create_dataset",
+    "magenta.models.pianoroll_rnn_nade.pianoroll_rnn_nade_generate",
+    "magenta.models.pianoroll_rnn_nade.pianoroll_rnn_nade_train",
+    "magenta.models.polyphony_rnn.polyphony_rnn_create_dataset",
+    "magenta.models.polyphony_rnn.polyphony_rnn_generate",
+    "magenta.models.polyphony_rnn.polyphony_rnn_train",
+    "magenta.models.rl_tuner.rl_tuner_train",
+    "magenta.models.sketch_rnn.sketch_rnn_train",
+    "magenta.scripts.convert_dir_to_note_sequences",
+    "magenta.tensor2tensor.t2t_datagen",
+    "magenta.tensor2tensor.t2t_decoder",
+    "magenta.tensor2tensor.t2t_trainer",
 ]
 # pylint:enable=line-too-long
 
 setup(
-    name='magenta',
+    name="magenta",
     version=__version__,  # pylint: disable=undefined-variable
-    description='Use machine learning to create art and music',
-    long_description='',
-    url='https://magenta.tensorflow.org/',
-    author='Google Inc.',
-    author_email='magenta-discuss@gmail.com',
-    license='Apache 2',
+    description="Use machine learning to create art and music",
+    long_description="",
+    url="https://magenta.tensorflow.org/",
+    author="Google Inc.",
+    author_email="magenta-discuss@gmail.com",
+    license="Apache 2",
     # PyPI package information.
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Education',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python :: 3',
-        'Topic :: Scientific/Engineering :: Mathematics',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Topic :: Software Development :: Libraries',
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering :: Mathematics",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Software Development :: Libraries",
     ],
-    keywords='tensorflow machine learning magenta music art',
-
+    keywords="tensorflow machine learning magenta music art",
     packages=find_packages(),
     install_requires=REQUIRED_PACKAGES,
     extras_require=EXTRAS_REQUIRE,
     entry_points={
-        'console_scripts': ['%s = %s:console_entry_point' % (n, p) for n, p in
-                            ((s.split('.')[-1], s) for s in CONSOLE_SCRIPTS)],
+        "console_scripts": [
+            "%s = %s:console_entry_point" % (n, p)
+            for n, p in ((s.split(".")[-1], s) for s in CONSOLE_SCRIPTS)
+        ],
     },
-
     include_package_data=True,
     package_data={
-        'magenta': ['models/image_stylization/evaluation_images/*.jpg'],
+        "magenta": ["models/image_stylization/evaluation_images/*.jpg"],
     },
 )
