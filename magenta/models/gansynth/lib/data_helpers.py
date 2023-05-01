78661d8c8de2460b63e8956b1f3f752415e81184
Rebecca Chen <rechen@google.com>
2023-05-01 10:34:26 -0700

    Silence some pytype errors.
    PiperOrigin-RevId: 528510090

--- magenta/models/gansynth/lib/data_helpers.py
+++ magenta/models/gansynth/lib/data_helpers.py
@@ -1,4 +1,4 @@
-# Copyright 2022 The Magenta Authors.
+# Copyright 2023 The Magenta Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -93,7 +93,7 @@ class DataSTFTHelper(DataHelper):
         mel_downscale=1,
         ifreq=True)
 
-  def _map_fn(self, wave, one_hot_label):
+  def _map_fn(self, wave, one_hot_label):  # pytype: disable=signature-mismatch  # overriding-parameter-count-checks
     waves = wave[tf.newaxis, :, :]
     data = self.waves_to_data(waves)
     return data[0], one_hot_label
@@ -143,7 +143,7 @@ class DataSTFTNoIFreqHelper(DataHelper):
         mel_downscale=1,
         ifreq=False)
 
-  def _map_fn(self, wave, one_hot_label):
+  def _map_fn(self, wave, one_hot_label):  # pytype: disable=signature-mismatch  # overriding-parameter-count-checks
     waves = wave[tf.newaxis, :, :]
     data = self.waves_to_data(waves)
     return data[0], one_hot_label
