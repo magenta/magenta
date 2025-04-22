5ee013071b119232e935f022caaa4aed3c1915ea
Magenta Team <no-reply@google.com>
2025-04-22 09:40:22 -0700

    Use the fact that sin(acos(x)) = sqrt(1 - x*x) to speed up slerp operation.\
    PiperOrigin-RevId: 750225529

--- magenta/models/gansynth/lib/generate_util.py
+++ magenta/models/gansynth/lib/generate_util.py
@@ -26,9 +26,11 @@ MAX_VELOCITY = 127.0
 
 def slerp(p0, p1, t):
   """Spherical linear interpolation."""
-  omega = np.arccos(np.dot(
-      np.squeeze(p0/np.linalg.norm(p0)), np.squeeze(p1/np.linalg.norm(p1))))
-  so = np.sin(omega)
+  cos_omega = np.dot(
+      np.squeeze(p0 / np.linalg.norm(p0)), np.squeeze(p1 / np.linalg.norm(p1))
+  )
+  omega = np.arccos(cos_omega)
+  so = np.sqrt(1 - cos_omega * cos_omega)
   return np.sin((1.0-t)*omega) / so * p0 + np.sin(t*omega)/so * p1
 
 
